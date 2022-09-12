b43f6c20fc6995ef0c2a5b3069a024740cb3c402
AntonPolyTech <COOLgay123@mail.ru>
2022-09-12 13:02:40 +0300

    Added design

--- Tyurin_Anton/main.c
+++ Tyurin_Anton/main.c
@@ -4,16 +4,16 @@
 int main() {
     int year = 20;  // количество лет, на которые взята ипотека
     double cr_percent = 8.0;  // процент ипотеки
-    double dep_percent = 5.2 / 12;
+    double dep_percent = 6 / 12;
     int flat_cost = (int)(9000000 * 100);  // стоимость квартиры
     int start = (int)(1000000 * 100);  // массив стартовый капитал
     int first_pay = (int)(300000 * 100);  // первый взнос
-    int salary = (int)(150000 * 100);  // зарплата
+    int a_salary = (int)(150000 * 100);  // зарплата Alice
+    int b_salary = (int)(150000 * 100);  // зарплата Bob
     int a_com_pay = (int)(40000 * 100);  // коммунальные услуги Alice
     int b_com_pay = (int)(10000 * 100);  // коммунальные услуги Bob
     int month_deposit = (int)(70000 * 100);  // ежемесячные отчисления на вклад
-    long double deposit, year_payment, year_deposit;
-    long double delta_deposit, month_payment;
+    long double deposit, year_payment, year_deposit,delta_deposit, month_payment,taxes;
 
     long double a_balance = start;  // активы Alice после покупки Bob квартиры
     long double a_balance_out = (double)a_balance / 100;
@@ -30,10 +30,10 @@ int main() {
 
 
     // вывод активов после покупки Bob квартиры:
-
-    printf("year %2d: ", 0);
-    printf("%14.2Lf%14.2Lf ", a_balance_out, b_balance_out);
-    printf("  dif = %14.2Lf\n", dif);
+    printf("Year|\t  Alice\t\t|\tBob\t| Difference|\n");
+    printf("%-4d|\t",0);
+    printf(" %10.2Lf\t|  %10.2Lf\t|", a_balance_out, b_balance_out);
+    printf(" %10.2Lf|\t\n", dif);
 
 
     for (int i = 1; i <= year; i++)
@@ -45,6 +45,16 @@ int main() {
 
         year_payment = 0;
         year_deposit = 0;
+
+        if(i==5)
+        {
+            b_balance+=flat_cost*0.5;
+        }
+        if(i==10)
+        {
+            b_salary-=5000000;
+        }
+        taxes=(double)flat_cost*(0.5/100);
 
         for(int j = 0; j < 12; j++)
         {
@@ -62,18 +72,18 @@ int main() {
             year_deposit += (int)delta_deposit;
         }
 
-        b_balance += (salary - b_com_pay) * 12 - year_payment + year_deposit;
+        b_balance += (b_salary - b_com_pay) * 12 - taxes - year_payment + year_deposit;
         b_balance_out = (double)b_balance / 100;
 
-        a_balance += (salary - a_com_pay) * 12 + year_deposit;
+        a_balance += (a_salary - a_com_pay) * 12 + year_deposit;
         a_balance_out = (double)a_balance / 100;
 
         dif = b_balance_out - a_balance_out;
 
 
-        printf("year %2d: ", i);
-        printf("%14.2Lf%14.2Lf ", a_balance_out, b_balance_out);
-        printf("  dif = %14.2Lf\n", dif);
+        printf("%-4d|\t", i);
+        printf(" %10.2Lf\t|  %10.2Lf\t| ", a_balance_out, b_balance_out);
+        printf("%10.2Lf|\t\n", dif);
     }
     return 0;
 }
