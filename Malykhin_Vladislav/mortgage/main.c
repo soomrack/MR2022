12e4068c42da38d2c5eea37578d0074c060cd9ef
mvofus <m.v.of@ya.ru>
2022-09-12 13:25:19 +0300

    PriceRise, income shortage, tax, deposit rate rise were added.

--- Malykhin_Vladislav/mortgage/main.c
+++ Malykhin_Vladislav/mortgage/main.c
@@ -23,18 +23,18 @@ long long priceRise(long long a){
 int main() {
     X = Y/12/20;
     Person Alice = {
-           100000000, 0, 0, 4000000, Y, 15000000, 0,Z,0,
+           10000000, 0, 0, 4000000, Y, 15000000, 0,Z,0,
    };
      Person Bob = {
             100000000, 30000000, X, 4000000, Y, 15000000, 0,Z,A
     };
     Bob.kapital -= Bob.perviyVznos;
     Bob.dolg += Bob.stoimostKv - Bob.perviyVznos;
-    printf("year 0 Alice %.2lf Bob %.2lf difference %.2lf \n",(double) Alice.kapital/100.0,(double) (Bob.kapital + 900000000)/100.0, (double) (Bob.kapital + 900000000 - Alice.kapital)/100.0);
+    printf("year:0  \t Alice:%.2lf \t Bob:%.2lf \t difference %.2lf \n",(double) Alice.kapital / 100.0,(double) (Bob.kapital + 900000000) / 100.0, (double) (Bob.kapital + 900000000 - Alice.kapital) / 100.0);
     for(int year = 0; year < years; year++) {
         for (int mounth = 0; mounth < mounths; mounth++) {
-            Alice.kapital += (long long) ((Alice.dohod - Alice.rashody) * (1 + 0.01 * Alice.stavkaVklada / 12));
-            Bob.kapital += (long long) ((Bob.dohod - Bob.rashody - (Bob.ezhVznos + Bob.dolg * 0.01 * Bob.stavkaIpoteki / 12)) * (1 + 0.01 * Bob.stavkaVklada / 12));
+            Alice.kapital +=  (Alice.dohod - Alice.rashody) * (1 + 0.01 * Alice.stavkaVklada / 12);
+            Bob.kapital += (Bob.dohod - Bob.rashody - (Bob.ezhVznos + Bob.dolg * 0.01 * Bob.stavkaIpoteki / 12)) * (1 + 0.01 * Bob.stavkaVklada / 12);
             Bob.dolg -= Bob.ezhVznos;
         }
         if ( year == 2){
@@ -46,10 +46,10 @@ int main() {
             Bob.kapital += priceRise(Bob.stoimostKv);
         }
         if(year == 6){
-            Alice.dohod -=30000;
-            Bob.dohod -= 30000;
+            Alice.dohod -=3000000;
+            Bob.dohod -= 3000000;
         }
         Bob.kapital-= tax(Bob.stoimostKv);
-        printf("year %d Alice %.2lf Bob %.2lf difference %.2lf \n",year+1, (double) Alice.kapital/100.0, (double) (Bob.kapital + 900000000)/100.0, (double) (Bob.kapital + 900000000 - Alice.kapital)/100.0);
+        printf("year:%d  \t Alice:%.2lf \t Bob:%.2lf \t difference:%.2lf \n",year+1, (double) Alice.kapital/100.0, (double) (Bob.kapital + 900000000) / 100.0, (double) (Bob.kapital + 900000000 - Alice.kapital) / 100.0);
     }
 }
