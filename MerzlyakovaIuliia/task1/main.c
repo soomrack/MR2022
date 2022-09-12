d9b6130fdb6ba62a0dd24739485781d024591b8b
ShchelkanovaValentina <tshelkanova.va@edu.spbstu.ru>
2022-09-12 11:40:09 +0300

    Merge branch 'main' of https://github.com/soomrack/MR2022

--- MerzlyakovaIuliia/task1/main.c
+++ MerzlyakovaIuliia/task1/main.c
@@ -3,29 +3,32 @@
 #define DEB_PERCENT 0.08
 #define SAL_ALICE 150000.0
 #define SAL_BOB 150000.0
+#define BOB_FLAT_PAYMENT 100000.0
 #define COMM_ALICE 40000.0
 #define COMM_BOB 10000.0
 #define OVERALL_TIME 20
 #define DEPOSIT 300000
 
-int main() {
+int main()
+{
     float bankAlice = 1000000, bankBob = 1000000;
     bankBob -= DEPOSIT;
 
-    for(int i = 0; i < OVERALL_TIME; i++)
+    for(int i = 0; i < (OVERALL_TIME*12); i++)
     {
         bankAlice += SAL_ALICE - COMM_ALICE;
-        bankAlice += bankAlice * DEB_PERCENT;
-        bankBob += SAL_BOB - COMM_BOB;
-        bankBob += bankBob * DEB_PERCENT;
+        bankAlice += bankAlice * (DEB_PERCENT/12);
+        bankBob += SAL_BOB - COMM_BOB - BOB_FLAT_PAYMENT;
+        bankBob += bankBob * (DEB_PERCENT/12);
     }
 
     printf("Alice's money: %f\n", bankAlice);
     printf("Bob's money: %f\n", bankBob);
 
-    if(bankAlice>bankBob) printf("Alice has more money %f\n");
-    else printf("\nBob has more money %f\n");
+    if(bankAlice>bankBob)
+        printf("\nAlice has more money\n");
+    else
+        printf("\nBob has more money\n");
 
-   // printf("Hello, World!\n");
     return 0;
 }
