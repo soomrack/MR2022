762b1591a16eb13a7446634d4112d238fac1c8cb
artem-kondartew <artemkondratev5@gmail.com>
2022-09-30 13:05:55 +0300

    new_matrix() fixed (killed nan), matrix_multiplication() added

--- Kondratev_Artem/matrix.c
+++ Kondratev_Artem/matrix.c
@@ -9,7 +9,7 @@ typedef struct Matrix {
     uint64_t rows;
     uint64_t cols;
 
-    long double **array;
+    double **array;
 
 } Matrix;
 
@@ -21,15 +21,15 @@ Matrix matrix_init(uint64_t rows, uint64_t cols) {
     matrix.rows = rows;
     matrix.cols  = cols;
 
-    uint64_t array_of_pointers_mem = rows * sizeof(long double *);
-    matrix.array = (long double **)malloc(array_of_pointers_mem);
+    matrix.array = (double**)malloc(rows * sizeof(double*));
 
-    uint64_t column_array_mem = (cols + 1) * sizeof(long double *);
-    for(int row = 0; row <= rows-1; row++)
-        matrix.array[row] = (long double *)malloc(column_array_mem);
+    for(int row = 0; row <= rows - 1; row++) {
+        matrix.array[row] = (double*)malloc(cols * sizeof(double));
+    }
 
-    free(malloc(array_of_pointers_mem));
-    free(malloc(column_array_mem));
+
+
+
 
     return matrix;
 
@@ -42,7 +42,7 @@ void filling_matrix(Matrix *matrix) {
 
     for(int row = 0; row <= matrix->rows-1; row++)
         for(int col = 0; col <= matrix->cols-1; col++)
-            scanf("%Lf", &matrix->array[row][col]);
+            scanf("%lf", &matrix->array[row][col]);
 
 }
 
@@ -52,7 +52,7 @@ void output_matrix(Matrix *matrix) {
     for(int row = 0; row <= matrix->rows-1; row++) {
 
         for(int col = 0; col <= matrix->cols-1; col++)
-            printf("%6.2Lf", matrix->array[row][col]);
+            printf("%6.2lf", matrix->array[row][col]);
 
         printf("\n");
     }
@@ -64,11 +64,12 @@ Matrix new_matrix() {
 
     uint64_t  rows, cols;
 
-    printf("input new matrix parameters:\n");
+    printf("input new matrix sizes:\n");
     scanf("%lux%lu", &rows, &cols);
-    printf("%lux%lu\n", rows, cols);
+    printf("sizes: %lux%lu\n", rows, cols);
 
     Matrix matrix = matrix_init(rows, cols);
+
     filling_matrix(&matrix);
 
     printf("You entered:\n");
@@ -77,9 +78,6 @@ Matrix new_matrix() {
     return matrix;
 
 }
-
-
-
 
 
 int matrix_size_comparer(Matrix *matrix1, Matrix *matrix2) {
@@ -107,27 +105,46 @@ int matrix_addition() {
         return -1;
     }
 
-    printf("You get:\n");
     Matrix sum_matrix = matrix_init(matrix1.rows, matrix1.rows);
 
     for(int row = 0; row <= matrix1.rows-1; row++)
         for(int col = 0; col <= matrix1.cols-1; col++)
             sum_matrix.array[row][col] = matrix1.array[row][col] + matrix2.array[row][col];
 
+    printf("You get:\n");
     output_matrix(&sum_matrix);
 }
 
 
 void matrix_multiplication() {
 
+    Matrix matrix1 = new_matrix();
 
+    Matrix matrix2 = new_matrix();
+
+    Matrix multiplied_matrix = matrix_init(matrix1.rows, matrix2.cols);
+
+    int key = (int)matrix1.cols;
+
+    for(int row = 0; row <= multiplied_matrix.rows - 1; row++)
+
+        for(int col = 0; col <= multiplied_matrix.cols - 1; col++) {
+
+            multiplied_matrix.array[row][col] = 0;
+
+            for (int k = 0; k <= key - 1; k++)
+                multiplied_matrix.array[row][col] += matrix1.array[row][k] * matrix2.array[k][col];
+        }
+
+    printf("You get:\n");
+    output_matrix(&multiplied_matrix);
 
 }
 
 
 int main() {
 
-    matrix_addition();
+    matrix_multiplication();
 
     return 0;
 }
