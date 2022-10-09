2664c9ad9d1de0bb87a8f925d9df77184a7dddf8
AntonPolyTech <COOLgay123@mail.ru>
2022-10-09 19:50:30 +0300

    fuctions, output and test added

--- Tyurin_Anton/main.c
+++ Tyurin_Anton/main.c
@@ -59,39 +59,72 @@ void print_matrix(const struct Matrix X){// Вывод матрицы на эк�
     }
 }
 
+
 struct Matrix error(void){
     struct Matrix error;
     error.cols = 2;
-    error.rows = 2;
+    error.rows = 1;
     error.values = malloc(error.cols * error.rows * sizeof(double));
+    printf("Wrong configuration\n");
     for (unsigned int idx = 0; idx < error.cols * error.rows; idx++){
         error.values[idx] = 0.0;
+        printf("%.1lf\t",error.values[idx]);
     }
-
+    printf("\n");
     return error;
 }
 
+
+struct Matrix matrix_output(const struct Matrix A,char symbol) {
+    printf("A %c B=\n", symbol);
+    for (unsigned int row = 0; row < A.rows; row++) {
+        for (unsigned int col = 0; col < A.cols; col++) {
+            printf("%.1lf\t",A.values[row * A.cols + col]);
+        }
+        printf("\n");
+    }
+}
+
+
 struct Matrix matrix_sum(const struct Matrix A,const struct Matrix B){
     if ((A.cols != B.cols)*(A.rows != B.rows)) return error();
-        for (unsigned int row = 0; row < A.rows; row++) {
-            for (unsigned int col = 0; col < A.cols; col++) {
-                A.values[row * A.cols + col] += B.values[row * B.cols + col];
-            }
+    for (unsigned int row = 0; row < A.rows; row++) {
+        for (unsigned int col = 0; col < A.cols; col++) {
+            A.values[row * A.cols + col] += B.values[row * B.cols + col];
+
         }
+    }
+    matrix_output(A,'+');
     return A;
 }
 
-struct Matrix matrix_sub(const struct Matrix A, const struct Matrix B){
+
+struct Matrix matrix_sub(const struct Matrix A, const struct Matrix B) {
     if ((A.cols != B.cols)*(A.rows != B.rows)) return error();
     for (unsigned int row = 0; row < A.rows; row++) {
         for (unsigned int col = 0; col < A.cols; col++) {
             A.values[row * A.cols + col] -= B.values[row * B.cols + col];
         }
     }
+    matrix_output(A,'-');
     return A;
 }
 
+
+struct Matrix matrix_output1(struct Matrix C) {
+    printf("A * B=\n");
+    for (unsigned int row = 0; row < C.rows; row++) {
+        for (unsigned int col = 0; col < C.cols; col++) {
+            printf("%.1lf\t", C.values[col * C.cols + row]);
+        }
+        printf("\n");
+    }
+}
+
+
 struct Matrix matrix_mult(const struct Matrix A, const struct Matrix B) {
+
+    if ((A.cols != B.rows)+(A.rows != B.cols)) return error();
 
     struct Matrix C;
     C.cols = A.rows;
@@ -106,30 +139,31 @@ struct Matrix matrix_mult(const struct Matrix A, const struct Matrix B) {
             }
         }
     }
-
-        printf("A*B=\n");
-        for (unsigned int row = 0; row < C.rows; row++) {
-            for (unsigned int col = 0; col < C.cols; col++) {
-                printf("%.1lf\t", C.values[col * C.cols + row]);
-            }
-            printf("\n");
-    }
+    matrix_output1(C);
     return C;
 }
 
-
-
+void test(struct Matrix One, struct Matrix Zero){
+    matrix_sum(One,Zero);
+    matrix_sub(Zero,One);
+    matrix_mult(One,Zero);
+}
 
 
 int main() {
 
-    struct Matrix A = matrix_make(3,2,A);
-    double arr_A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+    struct Matrix Zero = zero(2,2);
+    struct Matrix One = one(2,2);
+
+    test(One,Zero);
+
+    struct Matrix A = matrix_make(3,3,A);
+    double arr_A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
     data_input(A, arr_A);
     print_matrix(A);
 
-    struct Matrix B = matrix_make(2,3,B);
-    double arr_B[] = {6.0, 5.0, 4.0, 3.0, 2.0, 1.0};
+    struct Matrix B = matrix_make(3,3,B);
+    double arr_B[] = {9.0,8.0,7.0,6.0, 5.0, 4.0, 3.0, 2.0, 1.0};
     data_input(B, arr_B);
     print_matrix(B);
 
