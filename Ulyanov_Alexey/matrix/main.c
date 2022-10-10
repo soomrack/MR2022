e1540b82a608a6591520a0a3cb1f05b1b4ffee54
Ульянов Алексей <ulalex.2002.2@mail.ru>
2022-10-10 10:29:24 +0300

    matrix data added

--- Ulyanov_Alexey/matrix/main.c
+++ Ulyanov_Alexey/matrix/main.c
@@ -6,25 +6,27 @@
 
 const unsigned int max_range = 51;
 
+
 struct Matrix {
     unsigned int rows;
     unsigned int cols;
 
-    double **values;
+    double *data;
+    double **value;
 };
 
-struct Matrix creation (unsigned int rows, unsigned int cols){
+struct Matrix creation(const unsigned int rows, const unsigned int cols){
+    struct Matrix math;
+    math.rows = rows;
+    math.cols = cols;
+    math.data = (double*)malloc(rows * cols * sizeof (double *));
+    math.value = (void**)malloc(rows * sizeof(void *));
 
-    struct Matrix rez;
-    rez.rows = rows;
-    rez.cols = cols;
-    double ** arrey = (double **)malloc(rows * sizeof(double *));
-    for (int row = 0; row < rows; row ++){
-        arrey[row] = (double *)malloc(cols * sizeof(double *));
+    for (unsigned int row = 0; row < rows; row++){
+        math.value[row] = math.data + row * cols * sizeof(void);
+
     }
-    rez.values = arrey;
-    return rez;
-
+    return math;
 }
 
 struct Matrix Empty(){
@@ -37,10 +39,8 @@ struct Matrix Empty(){
 struct Matrix Zero(unsigned int rows, unsigned int cols){
 
     struct Matrix rez = creation(rows, cols);
-    for (int row = 0; row < rows; row++){
-        for (int col = 0; col < cols; col++){
-            rez.values[row][col] = 0.0;
-        }
+    for (int i = 0; i < rows * cols; i++){
+        rez.data[i] = 0.0;
     }
     return rez;
 
@@ -50,7 +50,7 @@ struct Matrix Unit(unsigned int n){
 
     struct Matrix rez = Zero(n, n);
     for (int i = 0; i < n; i++){
-        rez.values[i][i] = 1.0;
+        rez.value[i][i] = 1.0;
     }
     return rez;
 
@@ -60,7 +60,7 @@ struct Matrix transponation(struct Matrix x){
     struct Matrix rez = creation(x.cols, x.rows);
     for (int row = 0; row < rez.rows; row++){
         for (int col = 0; col < rez.cols; col++){
-            rez.values[row][col] = x.values[col][row];
+            rez.value[row][col] = x.value[col][row];
         }
     }
     return rez;
@@ -70,22 +70,22 @@ struct Matrix minor(unsigned int cur_row, unsigned int cur_col, struct Matrix x)
     struct Matrix rez = creation( x.rows - 1, x.cols - 1);
     for (int row = 0; row < cur_row; row++){
         for (int col = 0; col < cur_col; col++){
-            rez.values[row][col] = x.values[row][col];
+            rez.value[row][col] = x.value[row][col];
         }
     }
     for (int row = 0; row < cur_row; row++){
         for (int col = cur_col; col < rez.cols; col++){
-            rez.values[row][col] = x.values[row][col+1];
+            rez.value[row][col] = x.value[row][col+1];
         }
     }
     for (int row = cur_row; row < rez.rows; row++){
         for (int col = 0; col < cur_col; col++){
-            rez.values[row][col] = x.values[row+1][col];
+            rez.value[row][col] = x.value[row+1][col];
         }
     }
     for (int row = cur_row; row < rez.rows; row++){
         for (int col = cur_col; col < rez.cols; col++){
-            rez.values[row][col] = x.values[row+1][col+1];
+            rez.value[row][col] = x.value[row+1][col+1];
         }
     }
     return rez;
@@ -95,10 +95,8 @@ struct Matrix addition(const struct Matrix x, const struct Matrix y){
     if ((x.cols != y.cols) || (x.rows != y.rows)) return Empty();
 
     struct Matrix rez = creation(x.rows, x.cols);
-    for (int row = 0; row < rez.rows; row++){
-        for (int col = 0; col < rez.cols; col++){
-            rez.values[row][col] = x.values[row][col] + y.values[row][col];
-        }
+    for (int i = 0; i < x.rows * x.cols; i++){
+        rez.data[i] = x.data[i] + y.data[i];
     }
     return rez;
 
@@ -108,10 +106,8 @@ struct Matrix subtraction(const struct Matrix x, const struct Matrix y){
     if ((x.cols != y.cols) || (x.rows != y.rows)) return Empty();
 
     struct Matrix rez = creation(x.rows, x.cols);
-    for (int row = 0; row < rez.rows; row++){
-        for (int col = 0; col < rez.cols; col++){
-            rez.values[row][col] = x.values[row][col] - y.values[row][col];
-        }
+    for (int i = 0; i < x.rows * x.cols; i++){
+        rez.data[i] = x.data[i] - y.data[i];
     }
     return rez;
 
@@ -123,9 +119,9 @@ struct Matrix multiplication(const struct Matrix x, const struct Matrix y){
     struct Matrix rez = creation(x.rows, y.cols);
     for (int row = 0; row < rez.rows; row++){
         for (int col = 0; col < rez.cols; col++){
-            rez.values[row][col] = 0.0;
+            rez.value[row][col] = 0.0;
             for (int k = 0; k < x.cols; k++){
-                rez.values[row][col] += x.values[row][k] * y.values[k][col];
+                rez.value[row][col] += x.value[row][k] * y.value[k][col];
             }
         }
     }
@@ -136,10 +132,8 @@ struct Matrix multiplication(const struct Matrix x, const struct Matrix y){
 struct Matrix multy_k(struct Matrix x, double k){
 
     struct Matrix rez = creation(x.rows, x.cols);
-    for (int row = 0; row < rez.rows; row++){
-        for (int col = 0; col < rez.cols; col++){
-            rez.values[row][col] = x.values[row][col] * k;
-        }
+    for (int i = 0; i < x.rows * x.cols; i++){
+        rez.data[i] = x.data[i] * k;
     }
     return rez;
 
@@ -149,13 +143,13 @@ double det(struct Matrix x){
     if (x.cols != x.rows) return *(double*)malloc(1 * sizeof(double *));
 
     if (x.cols == 1){
-        return x.values[0][0];
+        return x.value[0][0];
     } else {
         double rez = 0.0;
         for (int i = 0; i < x.cols; i++) {
             struct Matrix temp = minor(0, i, x);
-            rez += pow(-1, i) * x.values[0][i] * det(temp);
-            free(temp.values);
+            rez += pow(-1, i) * x.value[0][i] * det(temp);
+            free(temp.value);
         }
         return rez;
     }
@@ -170,8 +164,8 @@ struct Matrix reverse(struct Matrix x){
     for (int row = 0; row < rez.rows; row++){
         for (int col = 0; col < rez.cols; col++){
             struct Matrix temp = minor(row, col, x);
-            rez.values[row][col] = pow(-1, row + col) * det(temp);
-            free(temp.values);
+            rez.value[row][col] = pow(-1, row + col) * det(temp);
+            free(temp.value);
         }
     }
     return multy_k(transponation(rez), 1/deter);
@@ -183,7 +177,8 @@ struct Matrix pow_mat(struct Matrix x, unsigned int n){
 
     if (n == 0) return Unit(x.rows);
     struct Matrix rez = creation(x.rows, x.cols);
-    rez.values = x.values;
+    rez.data = x.data;
+    rez.value = x.value;
     for (int i = 1; i < n; i++){
         rez = multiplication(x, rez);
     }
@@ -194,7 +189,7 @@ struct Matrix pow_mat(struct Matrix x, unsigned int n){
 struct Matrix exponent(struct Matrix x, const unsigned int kol_slog){
     if (x.cols != x.rows) return Empty();
 
-    struct Matrix rez = creation(x.rows, x.cols);
+    struct Matrix rez = Zero(x.rows, x.cols);
     double a_n = 1;
     for (int i = 0; i < kol_slog; i++){
         rez = addition(rez, multy_k(pow_mat(x, i), a_n));
@@ -209,7 +204,7 @@ void output(struct Matrix x){
 
     for (int row = 0; row < x.rows; row++){
         for (int col = 0; col < x.cols; col++){
-            printf("%.2lf ", x.values[row][col]);
+            printf("%.2lf ", x.value[row][col]);
         }
         printf("\n");
     }
@@ -228,12 +223,12 @@ void test_of_add(){
 
     for (int row = 0; row < first.rows; row++){
         for (int col = 0; col < first.cols; col++) {
-            first.values[row][col] = arr_first[row][col];
+            first.value[row][col] = arr_first[row][col];
         }
     }
     for (int row = 0; row < second.rows; row++){
         for (int col = 0; col < second.cols; col++) {
-            second.values[row][col] = arr_second[row][col];
+            second.value[row][col] = arr_second[row][col];
         }
     }
 
@@ -242,7 +237,7 @@ void test_of_add(){
     int flag = (addition(Zero(2,2), first).rows == Empty().rows);
     for (int row = 0; row < rez_add.rows; row++){
         for (int col = 0; col < rez_add.cols; col++) {
-            flag *= (rez_add.values[row][col] == 7.0);
+            flag *= (rez_add.value[row][col] == 7.0);
         }
     }
 
@@ -262,12 +257,12 @@ void test_of_sub(){
 
     for (int row = 0; row < first.rows; row++){
         for (int col = 0; col < first.cols; col++) {
-            first.values[row][col] = arr_first[row][col];
+            first.value[row][col] = arr_first[row][col];
         }
     }
     for (int row = 0; row < second.rows; row++) {
         for (int col = 0; col < second.cols; col++) {
-            second.values[row][col] = arr_second[row][col];
+            second.value[row][col] = arr_second[row][col];
         }
     }
 
@@ -276,7 +271,7 @@ void test_of_sub(){
     int flag = (subtraction(Zero(2,2), Unit(3)).rows == Empty().rows);
     for (int row = 0; row < rez_sub.rows; row++){
         for (int col = 0; col < rez_sub.cols; col++) {
-            flag *= (rez_sub.values[row][col] == 0.0);
+            flag *= (rez_sub.value[row][col] == 0.0);
         }
     }
 
@@ -296,12 +291,12 @@ void test_of_multy(){
 
     for (int row = 0; row < first.rows; row++){
         for (int col = 0; col < first.cols; col++) {
-            first.values[row][col] = arr_first[row][col];
+            first.value[row][col] = arr_first[row][col];
         }
     }
     for (int row = 0; row < second.rows; row++) {
         for (int col = 0; col < second.cols; col++) {
-            second.values[row][col] = arr_second[row][col];
+            second.value[row][col] = arr_second[row][col];
         }
     }
 
@@ -310,7 +305,7 @@ void test_of_multy(){
     int flag = (multiplication(Zero(2,3), Unit(2)).rows == Empty().rows);
     for (int row = 0; row < rez_mul.rows; row++){
         for (int col = 0; col < rez_mul.cols; col++) {
-            flag *= (rez_mul.values[row][col] == 7.0);
+            flag *= (rez_mul.value[row][col] == 7.0);
         }
     }
 
@@ -326,7 +321,7 @@ void test_of_reverse(){
     struct Matrix obj = creation(n, n);
     for (int row = 0; row < obj.rows; row++){
         for (int col = 0; col < obj.cols; col++){
-            obj.values[row][col] = rand() % max_range;
+            obj.value[row][col] = rand() % max_range;
         }
     }
 
@@ -337,14 +332,14 @@ void test_of_reverse(){
 
     for (int row = 0; row < rez.rows; row++){
         for (int col = 0; col < rez.cols; col++){
-            flag *= (round(rez.values[row][col]) == unit_3.values[row][col]);
+            flag *= (round(rez.value[row][col]) == unit_3.value[row][col]);
         }
     }
 
     if (flag){
-        printf("Test of reverse matrix.c was successful\n");
+        printf("Test of reverse matrix was successful\n");
     } else {
-        printf("Test of reverse matrix.c was faild\n");
+        printf("Test of reverse matrix was faild\n");
     }
 }
 
@@ -352,7 +347,7 @@ void test_of_exponent(){
     struct Matrix base = creation(3, 3);
     for (int row = 0; row < base.rows; row++){
         for (int col = 0; col < base.cols; col++){
-            base.values[row][col] = rand() % max_range;
+            base.value[row][col] = rand() % max_range;
         }
     }
 
@@ -362,7 +357,7 @@ void test_of_exponent(){
     int flag = (option_1.rows == option_2.rows);
     for (int row = 0; row < option_1.rows; row++){
         for (int col = 0; col < option_1.cols; col++){
-            flag *= (option_1.values[row][col] == option_2.values[row][col]);
+            flag *= (option_1.value[row][col] == option_2.value[row][col]);
         }
     }
 
@@ -390,12 +385,12 @@ int main() {
 
     for (int i = 0; i < A.rows; i++){
         for (int j = 0; j < A.cols; j++){
-            A.values[i][j] = rand() % max_range;
+            A.value[i][j] = rand() % max_range;
         }
     }
     for (int i = 0; i < B.rows; i++){
         for (int j = 0; j < B.cols; j++){
-            B.values[i][j] = rand() % max_range;
+            B.value[i][j] = rand() % max_range;
         }
     }
 
@@ -403,34 +398,34 @@ int main() {
 
     printf("This is empty Matrix\n");
     output(Empty());
-    printf("This is Zero matrix.c\n");
+    printf("This is Zero matrix\n");
     output(Zero(5,5));
-    printf("This is Unit matrix.c\n");
+    printf("This is Unit matrix\n");
     output(Unit(5));
 
-    printf("This is matrix.c A\n");
+    printf("This is matrix A\n");
     output(A);
-    printf("This is matrix.c B\n");
+    printf("This is matrix B\n");
     output(B);
 
-    printf("This is transporation of matrix.c A\n");
+    printf("This is transporation of matrix A\n");
     output(transponation(A));
 
-    printf("This is matrix.c A + B\n");
+    printf("This is matrix A + B\n");
     output(addition(A,B));
-    printf("This is matrix.c A - B\n");
+    printf("This is matrix A - B\n");
     output(subtraction(A,B));
-    printf("This is matrix.c A * B\n");
+    printf("This is matrix A * B\n");
     output(multiplication(A, B));
-    printf("This is matrix.c A * 0.33\n");
+    printf("This is matrix A * 0.33\n");
     output(multy_k(A, 0.33));
 
-    printf("This is det of matrix.c A\n");
+    printf("This is det of matrix A\n");
     printf("%.2lf\n\n", det(A));
 
-    printf("This is reverse of matrix.c A\n");
+    printf("This is reverse of matrix A\n");
     output(reverse(A));
-    printf("This is matrix.c A in degree 3\n");
+    printf("This is matrix A in degree 3\n");
     output(pow_mat(A, 3));
 
     printf("This is exp of Matrix A\n");
