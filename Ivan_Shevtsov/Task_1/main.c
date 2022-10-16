69a7e372692e26443a5b0c76163806857c17d1c3
Ivan <81966696+I1sh@users.noreply.github.com>
2022-10-17 00:08:45 +0300

    Update main.c

--- Ivan_Shevtsov/Task_1/main.c
+++ Ivan_Shevtsov/Task_1/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
+double Alice_count();
+double Bob_count();
 double mortgage_pay();
 double deposit(int sum, int time_month, int add, double rate);
 
@@ -11,7 +13,6 @@ const int flat_beg = 0.8 * start_capital; //first mortgage pay (minimal value de
 const int flat_rent = 45 * 100000;
 const int expenses  = 40 * 100000;
 const int flat_price = 8 * 1000 * 100000;
-
 const double mortgage_rate = 0.109 / 12; //monthly
 const double inflation = 0.05 /12;
 
@@ -33,29 +34,10 @@ double Alice_count() {
 }
 
 double Bob_count() {
-    mortgage_pay();
     Bob.capital -= flat_beg;
     Bob.income -= mortgage_pay() + expenses;
     Bob.capital = deposit(Bob.capital, months, Bob.income, Bob.deposit_rate);
     return Bob.capital;
-}
-
-void output_results() {
-    printf("Alice: %d \n", Alice.capital);
-    printf("Bob: %d \n", Bob.capital);
-}
-
-void time_line() {
-
-    double new_flat_price = deposit(flat_price, months, 0, inflation);
-    printf("For %d months Alice managed to earn %1.lf thousand in the warehouse \n",  months, round(Alice_count() / 100000));
-    printf("And Bob earned %1.lf thousand on deposit, while his apartment was studying with inflation adjusted to %1.lf millions \n\n",
-           round(Bob_count() / 100000), round(new_flat_price / 100000000));
-    long long int Alice_sum = Alice.capital;
-    long long int Bob_sum = Bob.capital + new_flat_price;
-    printf("Alice sum = %1.lf millions\n Bob sum = %1.lf millions\n", round(Alice_sum / 100000000), round(Bob_sum / 100000000));
-
-
 }
 
 
@@ -65,12 +47,15 @@ double deposit(int sum, int time_month, int add, double rate) {
         deposit = (deposit + add) * (1 + rate);
         if (month == 5 * 12){
             Alice.income += 20 * 100000; // income upgrade in 5 year
-            Bob.income   += 20 * 100000;
+            Bob.income += 20 * 100000;
+            add = Alice.income;
         }
         if (month == 12 * 12){
             Alice.deposit_rate = 0.05 / 12; // deposit rate downgrade in 12 year
             Bob.deposit_rate   = 0.05 / 12;
         }
+    Alice.income = 150 * 100000;
+    Bob.income = 150 * 100000;
     }
     return deposit;
 }
@@ -83,6 +68,13 @@ double mortgage_pay() {
 
 
 int main() {
-    time_line();
+    long long int Alice_sum = Alice_count();
+    long long int Bob_sum = Bob_count();
+    double new_flat_price = deposit(flat_price, months, 0, inflation);
+    printf("For %d months Alice managed to earn %1.lf thousand in the warehouse \n",  months, round(Alice_sum / 100000));
+    printf("And Bob earned %1.lf thousand on deposit, while his apartment was studying with inflation adjusted to %1.lf millions \n\n",
+           round(Bob_sum / 100000), round(new_flat_price / 100000000));
+
+    printf("Alice sum = %1.lf millions\n  Bob sum = %1.lf millions\n", round(Alice_sum / 100000000), round((Bob_sum + new_flat_price) / 100000000));
     return 1;
 }
