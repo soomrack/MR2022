7f442df8c73f4a8f368cc527694a014d3edd7280
reiv27 <romaneidelman.offical@gmail.com>
2022-10-17 13:00:56 +0300

    memory allocation and array creation

--- EidelmanRoman/task02/task02.c
+++ EidelmanRoman/task02/task02.c
@@ -1,35 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **MatrixA;
-
 typedef struct matrix_size {
-    unsigned int lines;
-    unsigned int columns;
+    unsigned int rows;
+    unsigned int cols;
+    double **Matrix;
 } matrix_size;
 
-matrix_size size = {2, 2};
+matrix_size A = {2, 2};
 
-void array_creation_A {
-    MatrixA = (int **)malloc(size.lines * sizeof(int *));
-    for (int i = 0; i < lines; i++)
-        MatrixA[i] = (int *)malloc(size.columns * sizeof(int));
-    for (int i = 0; i < size.lines; i++)
+void array_creation(matrix_size *name)
+{
+    name->Matrix = (int**)malloc(name->rows * sizeof(int*));
+    for (int i = 0; i < name->rows; i++)
+        name->Matrix[i] = (int*)malloc(name->cols * sizeof(int));
+    for (int i = 0; i < name->rows; i++)
     {
-        for(int j = 0; j < size.columns)
+        for(int j = 0; j < name->cols; j++)
         {
-            MatrixA[i][j] = rand() % 9;
+            name->Matrix[i][j] = rand() % 9;
         }
     }
 }
 
+void output()
+{
+    for (int i = 0; i < A.rows; i++)
+    {
+        for(int j = 0; j < A.cols; j++)
+        {
+            printf("%lf ", A.Matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 
 void total() {
-    array_creation_A();
-    addition();
-    subtraction();
-    multiplication();
+    array_creation(&A);
+    //addition();
+    //subtraction();
+    //multiplication();
+    output();
 
 }
 
