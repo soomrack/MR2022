864791c0b8f1d21c29db4961fe9341de82da8b2d
AntonPolyTech <COOLgay123@mail.ru>
2022-10-31 13:24:09 +0300

    Matrix  C++

--- Tyurin_Anton/CPP/matrix.cpp
+++ Tyurin_Anton/CPP/matrix.cpp
@@ -7,23 +7,48 @@ private:
     unsigned int rows;
 public:
     double *values;
-    Matrix(const unsigned int cols_num, const unsigned int rows_num);
+    Matrix(const unsigned int cols_m, const unsigned int rows_m);
+    Matrix(const Matrix &A);
+    ~Matrix();
+    Matrix data_input(Matrix matrix, double arr[]);
 };
 
 
-Matrix::Matrix(const unsigned int cols_num, const unsigned int rows_num){  // Инициализация матриц
-        cols = cols_num;
-        rows = rows_num;
-        values = new double[cols * rows];  // malloc(cols * rows * sizeof(double));
+Matrix::Matrix(const unsigned int cols_m, const unsigned int rows_m){  // Инициализация матриц
+        cols = cols_m;
+        rows = rows_m;
+        values = new double[cols * rows];
         // delete[] matrix.values;
         for (unsigned int idx = 0; idx < cols * rows; idx++){
             values[idx] = 0.0;
         }
 }
 
+Matrix::Matrix(const Matrix &A) {
+    rows = A.rows;
+    cols = A.cols;
+    values = new double[rows * cols];
+    for (unsigned int idx = 0; idx < rows * cols; idx++) {
+        values[idx] = A.values[idx];
+    }
+}
+
+Matrix::~Matrix() {
+    delete[] values;
+}
+
+
+Matrix Matrix::data_input(Matrix matrix, double arr[]){
+        for (unsigned int idx = 0; idx < cols * rows; idx++){
+            values[idx] = arr[idx];
+        }
+        return matrix;
+}
 
 
 int main() {
-    Matix A(5,5);
+    Matrix A(3,3);
+    Matrix C(3,3);
+    Matrix B(A);
     return 0;
 }
