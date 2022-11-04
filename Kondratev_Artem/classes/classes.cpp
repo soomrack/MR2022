4b18c31c8cb48c0f25928143487e6d7f67f17745
artem-kondartew <artemkondratev5@gmail.com>
2022-11-04 13:06:26 +0300

    copy constructor fixed()

--- Kondratev_Artem/classes/classes.cpp
+++ Kondratev_Artem/classes/classes.cpp
@@ -1,151 +1,89 @@
 #include <iostream>
-#include <cstdint>
 
-#define EPSILON 0.0001
 
 class Matrix {
 private:
-    uint64_t rows = 0;
-    uint64_t cols = 0;
+    int rows;
+    int cols;
 public:
+    int size;
     double **values;
-    uint64_t size = 0;
+    double *start;
 
-    Matrix(uint64_t init_rows, uint64_t init_cols) {
-        if (init_rows > 0 and init_cols > 0) {
-            rows = init_rows;
-            cols = init_cols;
-            size = rows * cols;
-            values = new double *[rows];
-            for (int row = 0; row < rows; row++)
-                values[row] = new double[cols];
-        }
-    }
+    Matrix(int input_rows, int input_cols);
+    ~Matrix();
+    Matrix(Matrix const &matrix);
+    void output() const;
+    void filling(double number) const;
 
-    ~Matrix() {
-        for (int row = 0; row < rows; row++)
-            delete values[row];
-        delete values;
-        rows = 0;
-        cols = 0;
-        size = 0;
-    }
+    Matrix operator = (Matrix &matrix);
+};
 
-    Matrix(const Matrix &matrix) {
-        values = new double *[rows];
-        for (int row = 0; row < rows; row++)
-            values[row] = new double[cols];
-        for (int row = 0; row <= matrix.rows-1; row++)
-            for (int col = 0; col <= matrix.cols-1; col++)
-                values[row][col] = matrix.values[row][col];
-    }
 
-    void filling(double number) const {
-        for (int row = 0; row < rows; row++)
-            for (int col = 0; col < cols; col++)
-                values[row][col] = number;
-    }
+Matrix::Matrix(int input_rows, int input_cols) {
+    rows = input_rows;
+    cols = input_cols;
+    size = rows * cols;
+    values = new double *[rows];
+    start = new double [size];
+    for (int row = 0; row < rows; row++)
+        values[row] = start + row * cols;
+}
 
-    void filling(const double array[]) const {
-        int array_index = 0;
-        for (int row = 0; row < rows; row++)
-            for (int col = 0; col < cols; col++) {
-                values[row][col] = array[array_index];
-                array_index += 1;
-            }
-    }
 
-    void output() const {
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++)
-                std::cout << values[row][col] << "  ";
-            std::cout << "\n";
-        }
+Matrix::~Matrix() {
+    rows = 0;
+    cols = 0;
+    size = 0;
+    delete values;
+    delete start;
+}
+
+
+Matrix::Matrix(Matrix const &matrix) {
+    rows = matrix.rows;
+    cols = matrix.cols;
+    size = rows * cols;
+    values = new double *[rows];
+    start = new double[size];
+    for (int row = 0; row < rows; row++)
+        values[row] = start + row * cols;
+    for (int cell = 0; cell < size; cell++)
+        start[cell] = matrix.start[cell];
+}
+
+
+void Matrix::output() const {
+    for (int row = 0; row < rows; row++) {
+        for (int col = 0; col < cols; col++)
+            std::cout << values[row][col] << "  ";
         std::cout << "\n";
     }
+    std::cout << "\n";
+}
 
-    [[nodiscard]] inline int is_len_zero() const {
-        return (int) (rows + cols);
-    }
 
-    static Matrix addition(Matrix matrix1, Matrix matrix2) {
-        /*if(matrix1.rows != matrix2.rows or matrix1.cols != matrix2.cols or
-                matrix1.are_row_col_not_zero() == 0 or matrix2.is_len_zero() == 0) {
-            Matrix error(0, 0);
-            return error;
-        }*/
-        //Matrix sum_matrix(matrix1.rows, matrix1.cols);
-        for (int row = 0; row < matrix1.rows; row++)
-            for (int col = 0; col < matrix1.cols; col++)
-                //sum_matrix.values[row][col] = matrix1.values[row][col] + matrix2.values[row][col];
-                //return sum_matrix;
-                int a;
+void Matrix::filling(double number) const {
+    for (int cell = 0; cell < size; cell++)
+        start[cell] = number;
+}
 
-    }
 
-    static Matrix addition(Matrix matrix1, double number) {
-        if (matrix1.is_len_zero() == 0) {
-            Matrix error(0, 0);
-            return error;
-        }
-        Matrix sum_matrix(matrix1.rows, matrix1.cols);
-        for (int row = 0; row < sum_matrix.rows; row++)
-            for (int col = 0; col < sum_matrix.cols; col++)
-                sum_matrix.values[row][col] = matrix1.values[row][col] + number;
-        return sum_matrix;
-    }
+Matrix ret(Matrix matrix) {
+    return matrix;
+}
 
-    static Matrix subtraction(Matrix matrix1, Matrix matrix2) {
-        if (matrix1.rows != matrix2.rows or matrix1.cols != matrix2.cols or matrix1.rows == 0 or matrix1.rows) {
-            Matrix error(0, 0);
-            return error;
-        }
-        Matrix sum_matrix(matrix1.rows, matrix1.cols);
-        for (int row = 0; row < sum_matrix.rows; row++)
-            for (int col = 0; col < sum_matrix.cols; col++)
-                sum_matrix.values[row][col] = matrix1.values[row][col] - matrix2.values[row][col];
-        return sum_matrix;
-    }
-
-    static Matrix multiplication(Matrix matrix1, Matrix matrix2) {
-        if (matrix1.cols != matrix2.rows or matrix1.is_len_zero() == 0 or matrix2.is_len_zero() == 0) {
-            Matrix error(0, 0);
-            return error;
-        }
-        Matrix multiplied_matrix(matrix1.rows, matrix2.cols);
-        for (int row = 0; row < multiplied_matrix.rows; row++)
-            for (int col = 0; col < multiplied_matrix.cols; col++) {
-                multiplied_matrix.values[row][col] = 0;
-                for (int k = 0; k < matrix1.cols; k++)
-                    multiplied_matrix.values[row][col] += matrix1.values[row][k] * matrix2.values[k][col];
-            }
-        return multiplied_matrix;
-    }
-
-    static Matrix multiplication(Matrix matrix1, double number) {
-        if (matrix1.is_len_zero() == 0) {
-            Matrix error(0, 0);
-            return error;
-        }
-        Matrix sum_matrix(matrix1.rows, matrix1.cols);
-        for (int row = 0; row < sum_matrix.rows; row++)
-            for (int col = 0; col < sum_matrix.cols; col++)
-                sum_matrix.values[row][col] = matrix1.values[row][col] * number;
-        return sum_matrix;
-    }
-
-};
 
 int main() {
 
-    Matrix A(2, 2);
+    Matrix A(2,2);
     A.filling(2);
-
-    Matrix B(2, 2);
-    B.filling(2);
-
-    //Matrix C(2, 2);
-    Matrix::addition(A, B);
+    A.output();
+    Matrix C = ret(A);
+    C.output();
+    A.filling(3);
+    A.output();
+    C.output();
 
     return 0;
 }
