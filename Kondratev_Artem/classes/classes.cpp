6512c937078e76cccc7158671d4a4787e9f2ed4c
artem-kondartew <artemkondratev5@gmail.com>
2022-11-04 14:09:22 +0300

    + overload added

--- Kondratev_Artem/classes/classes.cpp
+++ Kondratev_Artem/classes/classes.cpp
@@ -16,7 +16,9 @@ public:
     void output() const;
     void filling(double number) const;
 
-    Matrix operator = (Matrix &matrix);
+    //Matrix operator = (double i);
+    Matrix &operator = (Matrix const &matrix);
+    Matrix operator + (Matrix matrix) const;
 };
 
 
@@ -54,6 +56,7 @@ Matrix::Matrix(Matrix const &matrix) {
 
 
 void Matrix::output() const {
+
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++)
             std::cout << values[row][col] << "  ";
@@ -69,8 +72,29 @@ void Matrix::filling(double number) const {
 }
 
 
-Matrix ret(Matrix matrix) {
-    return matrix;
+/*Matrix Matrix::operator = (double i) {
+
+}*/
+
+
+Matrix &Matrix::operator = (Matrix const &matrix) {
+    rows = matrix.rows;
+    cols = matrix.cols;
+    size = rows * cols;
+    values = new double *[rows];
+    start = new double[size];
+    for (int row = 0; row < rows; row++)
+        values[row] = start + row * cols;
+    for (int cell = 0; cell < size; cell++)
+        start[cell] = matrix.start[cell];
+}
+
+
+Matrix Matrix::operator + (Matrix const matrix) const {
+    Matrix sum_matrix(2,2);
+    for (int cell = 0; cell < size; cell++)
+        sum_matrix.start[cell] = start[cell] + matrix.start[cell];
+    return sum_matrix;
 }
 
 
@@ -79,11 +103,14 @@ int main() {
     Matrix A(2,2);
     A.filling(2);
     A.output();
-    Matrix C = ret(A);
+
+    Matrix C(2,2);
+    C.filling(4);
     C.output();
-    A.filling(3);
-    A.output();
-    C.output();
+
+
+    Matrix K = C + A;
+    K.output();
 
     return 0;
 }
