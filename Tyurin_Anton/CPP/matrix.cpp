cc0f5c29763198a7b007060837de0c273e92aa28
AntonPolyTech <COOLgay123@mail.ru>
2022-11-14 12:16:52 +0300

    Matrix  C++

--- Tyurin_Anton/CPP/matrix.cpp
+++ Tyurin_Anton/CPP/matrix.cpp
@@ -8,9 +8,15 @@ public:
     double *values;
     Matrix(unsigned int cols_m, unsigned int rows_m);
     Matrix(const Matrix &A);
+    Matrix(unsigned int cols_m);
     ~Matrix();
-    static Matrix data_input(Matrix matrix, const double arr[]);
+    static Matrix data_input(Matrix *matrix, const double arr[]);
     static void print_matrix(Matrix matrix);
+    static void print_matrix(Matrix matrix, char symbol);
+    Matrix error();
+    Matrix operator+(Matrix X);
+    Matrix operator-(Matrix X);
+    Matrix operator*(Matrix X);
 };
 
 
@@ -37,11 +43,11 @@ Matrix::~Matrix() {
     delete[] values;
 }
 
-Matrix Matrix::data_input(Matrix matrix, const double arr[]){
-        for (unsigned int idx = 0; idx < matrix.cols * matrix.rows; idx++){
-            matrix.values[idx] = arr[idx];
+Matrix Matrix::data_input(Matrix *matrix, const double arr[]){
+        for (unsigned int idx = 0; idx < matrix->cols * matrix->rows; idx++){
+            matrix->values[idx] = arr[idx];
         }
-        return matrix;
+        return *matrix;
 }
 
 void Matrix::print_matrix(Matrix X) {  // Вывод матрицы на экран
@@ -53,10 +59,84 @@ void Matrix::print_matrix(Matrix X) {  // Вывод матрицы на экр�
         std::cout << "\n";
     }
 }
+
+void Matrix::print_matrix(Matrix matrix, char symbol){
+    std::cout << "Matrix";
+    switch(symbol){
+        case '+': std::cout << "\tsumm\n"; break;
+        case '-': std::cout << "\tsubstruction\n"; break;
+        case '*': std::cout << "\tmultiplication\n"; break;
+    }
+    for (unsigned int row = 0; row < matrix.rows; row++) {
+        for (unsigned int col = 0; col < matrix.cols; col++) {
+            std::cout << matrix.values[row * matrix.cols + col] << "\t";
+        }
+        std::cout << "\n";
+    }
+}
+
+Matrix::Matrix(unsigned int cols_m) {  // Конструктор единичной матрицы
+    cols = cols_m;
+    rows = cols_m;
+    values = new double[cols * rows];
+    // delete[] matrix.values;
+    for (unsigned int row = 0; row < rows; row++) {
+        for (unsigned int col = 0; col < cols; col++) {
+            values[row * cols + col] = (row == col) ? 1 : 0;
+
+        }
+    }
+}
+
+Matrix Matrix::error(){
+    Matrix error(0,0);
+    error.values = NULL;
+    return error;
+}
+
+Matrix Matrix::operator+ (Matrix X) {
+    Matrix sum(X.cols, X.rows);
+    for (unsigned int idx = 0; idx < X.cols * X.rows; idx++) {
+      sum.values[idx] = values[idx] + X.values[idx];
+    }
+    return sum;
+}
+
+Matrix Matrix::operator- (Matrix X) {
+    Matrix sub(X.cols, X.rows);
+    for (unsigned int idx = 0; idx < X.cols * X.rows; idx++) {
+        sub.values[idx] = values[idx] - X.values[idx];
+    }
+    return sub;
+}
+
+Matrix Matrix::operator* (Matrix X) {
+    Matrix mult(X.cols, X.rows);
+    for (unsigned int row = 0; row < rows; row++) {
+        for (unsigned int col = 0; col < X.cols; col++) {
+            mult.values[row * mult.rows + col] = 0;
+            for (unsigned int k = 0; k < cols; k++) {
+                mult.values[row * mult.cols + col] += values[row * cols + k] * X.values[k * X.cols + col];
+            }
+            return mult;
+        }
+    }
+}
+
 int main() {
     Matrix A(3,3);
     double arr_A[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0};
-    Matrix::data_input(A,arr_A);
+    Matrix::data_input(&A,arr_A);
+    Matrix B(3);
     Matrix::print_matrix(A);
+    Matrix::print_matrix(B);
+    Matrix Sum = A + B;
+    Matrix Sub = A - B;
+    Matrix Mult = A * B;
+
+    Matrix::print_matrix(Sum,'+');
+    Matrix::print_matrix(Sub,'-');
+    Matrix::print_matrix(Mult,'*');
+
     return 0;
 }
