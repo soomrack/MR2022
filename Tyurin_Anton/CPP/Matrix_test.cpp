a91f485f8bc8f51b2c820a3eb10dff8255668ee9
AntonPolyTech <COOLgay123@mail.ru>
2022-11-21 12:33:56 +0300

    Matrix  C++

--- Tyurin_Anton/CPP/Matrix_test.cpp
+++ Tyurin_Anton/CPP/Matrix_test.cpp
@@ -17,18 +17,41 @@ int Matrix_test::summ_test() {
     Matrix A(3,3);
     A.fill_with(2);
     Matrix B(3);
+    Matrix C(3,3);
+    double arr_C[] = {3.0, 2.0, 2.0, 2.0, 3.0, 2.0, 2.0, 2.0, 3.0};
+    Matrix::data_input(&C,arr_C);
     Matrix Sum = A + B;
     if (Sum.is_equal(C)){
-
+        return 1;
     }
     return 0;
 }
 
 int Matrix_test::sub_test() {
+    Matrix A(3,3);
+    A.fill_with(2);
+    Matrix B(3);
+    Matrix C(3,3);
+    double arr_C[] = {1.0, 2.0, 2.0, 2.0, 1.0, 2.0, 2.0, 2.0, 1.0};
+    Matrix::data_input(&C,arr_C);
+    Matrix Sub = A - B;
+    if (Sub.is_equal(C)){
+        return 1;
+    }
     return 0;
 }
 
 int Matrix_test::mult_test() {
+    Matrix A(3,3);
+    A.fill_with(2);
+    Matrix B(3);
+    Matrix C(3,3);
+    double arr_C[] = {3.0, 2.0, 2.0, 2.0, 3.0, 2.0, 2.0, 2.0, 3.0};
+    Matrix::data_input(&C,arr_C);
+    Matrix Mult = A * B;
+    if (Mult.is_equal(C)){
+        return 1;
+    }
     return 0;
 }
 
