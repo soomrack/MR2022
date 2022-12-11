e80b09ac8de1d339d218664222ed1eb6de0b91e7
AntonPolyTech <COOLgay123@mail.ru>
2022-12-11 18:59:07 +0300

    Matrix  Template

--- Tyurin_Anton/Template/Matrix_exception.h
+++ Tyurin_Anton/Template/Matrix_exception.h
@@ -7,12 +7,12 @@
 #ifndef MATRIX_EXCEPTION_H
 #define MATRIX_EXCEPTION_H
 
-template <typename T3>
+template <typename T>
 class MatrixException: public std::domain_error {
 private:
-    T3 message;
+    T message;
 public:
-    explicit MatrixException(const T3) : std::domain_error(message)
+    explicit MatrixException(const T) : std::domain_error(message)
     {}
     [[nodiscard]] std::string get_message() const;
     void print_message(const std::string& text, const MatrixException& Exception_object);
--- Tyurin_Anton/Template/Matrix_test.cpp
+++ Tyurin_Anton/Template/Matrix_test.cpp
@@ -2,75 +2,75 @@
 #include "Matrix.h"
 #include "Matrix_exception.h"
 
-template <typename T1, typename T2, typename T3>
-void Matrix_test <T2>::Matrix_test <T2>::summ_test() {
+template <typename T>
+void Matrix_test <T>::Matrix_test::summ_test() {
     try {
-        T1 A(3,3);
+        T A(3,3);
         A.fill_with(2);
-        T1 B(3);
-        T1 C(3, 3);
+        T B(3);
+        T C(3, 3);
         double arr_C[] = {3.0, 2.0, 2.0, 2.0, 3.0, 2.0, 2.0, 2.0, 3.0};
-        Matrix<T1>::Matrix::data_input(&C, arr_C);
-        T1 Sum = A + B;
+        Matrix<T>::Matrix::data_input(&C, arr_C);
+        T Sum = A + B;
         Sum.is_equal(C);
     }
-    catch(T3& exception){
+    catch(T& exception){
         exception.print_message("Error",exception);
     }
 }
 
-template <typename T2>
-void Matrix_test <T2>::Matrix_test::sub_test() {
+template <typename T>
+void Matrix_test <T>::Matrix_test::sub_test() {
     try {
-        Matrix A(3, 3);
+        T A(3, 3);
         A.fill_with(2);
-        Matrix B(3);
-        Matrix C(3, 3);
+        T B(3);
+        T C(3, 3);
         double arr_C[] = {1.0, 2.0, 2.0, 2.0, 1.0, 2.0, 2.0, 2.0, 1.0};
-        Matrix::data_input(&C, arr_C);
-        Matrix Sub = A - B;
+        Matrix<T>::data_input(&C, arr_C);
+        T Sub = A - B;
         Sub.is_equal(C);
     }
-    catch(MatrixException& exception){
+    catch(T& exception){
         exception.print_message("Error",exception);
     }
 }
 
-template <typename T2>
-void Matrix_test <T2>::Matrix_test::mult_test() {
+template <typename T>
+void Matrix_test <T>::Matrix_test::mult_test() {
     try {
-        Matrix A(3, 3);
+        T A(3, 3);
         A.fill_with(2);
-        Matrix B(3, 3);
+        T B(3, 3);
         B.fill_with(3);
-        Matrix C(3, 3);
+        T C(3, 3);
         C.fill_with(18);
-        Matrix Mult = A * B;
+        T Mult = A * B;
         Mult.is_equal(C);
     }
-    catch(MatrixException& exception){
+    catch(T& exception){
         exception.print_message("Error",exception);
     }
 }
 
-template <typename T2>
-void Matrix_test <T2>::Matrix_test::exp_test() {
+template <typename T>
+void Matrix_test <T>::Matrix_test::exp_test() {
     try {
-        Matrix A(3, 3);
+        T A(3, 3);
         A.fill_with(2);
-        Matrix real_exp(3, 3);
+        T real_exp(3, 3);
         double arr_real_exp[] = {129.411, 128.411, 128.411, 128.411, 129.411, 128.411, 128.411, 128.411, 129.411};
-        Matrix::data_input(&real_exp, arr_real_exp);
-        Matrix exponent = Matrix::exp(A);
+        T::data_input(&real_exp, arr_real_exp);
+        T exponent = Matrix<T>::exp(A);
         exponent.is_equal(real_exp);
     }
-    catch(MatrixException& exception){
+    catch(T& exception){
         exception.print_message("Error",exception);
     }
 }
 
-template <typename T2>
-void Matrix_test <T2>::Matrix_test::test(){
+template <typename T>
+void Matrix_test <T>::Matrix_test::test(){
     summ_test();
     sub_test();
     mult_test();
