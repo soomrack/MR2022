4d0470525c9b342f8b21afbe119f8102f24b7d15
artem-kondartew <artemkondratev5@gmail.com>
2022-12-12 10:33:44 +0300

    Fully rebuilt

--- Tyurin_Anton/Template/Matrix_exception.cpp
+++ Tyurin_Anton/Template/Matrix_exception.cpp
@@ -1,10 +1,11 @@
 #include "Matrix_exception.h"
 
-template<typename T>
- std::string MatrixException <T>::MatrixException::get_message() const {
+
+ std::string MatrixException::MatrixException::get_message() const {
     return message;
 }
-template<typename T>
-void MatrixException <T>::MatrixException::print_message(const std::string &text, const MatrixException &Exception_object) {
+
+
+void MatrixException::MatrixException::print_message(const std::string &text, const MatrixException &Exception_object) {
     std::cout << text << ":" << std::endl << Exception_object.get_message() << std::endl << std::endl;
 }
