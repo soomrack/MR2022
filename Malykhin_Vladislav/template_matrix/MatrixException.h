de723af3a9f7256cd0398173f3d566fe46cc3412
Vladislav <113335402+mvofus@users.noreply.github.com>
2022-12-20 16:37:04 +0300

    Add files via upload
    Start project

---
+++ Malykhin_Vladislav/template_matrix/MatrixException.h
@@ -0,0 +1,25 @@
+//
+// Created by User on 05.12.2022.
+//
+
+#ifndef MR2022_MATRIXEXCEPTION_H
+#define MR2022_MATRIXEXCEPTION_H
+
+#include <iostream>
+
+
+class MatrixException: public std::exception {
+private:
+    std::string message;
+public:
+    explicit MatrixException(std::string input_message);
+    MatrixException(const MatrixException &other) = default;
+    MatrixException(MatrixException &&other) = default;
+    ~MatrixException() override = default;
+
+    std::string get_message() const;
+};
+
+
+
+#endif //MR2022_MATRIXEXCEPTION_H
