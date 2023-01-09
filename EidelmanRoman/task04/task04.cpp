86d127f472e45a2e4721470637104de28cc6e50b
reiv27 <romaneidelman.offical@gmail.com>
2023-01-09 17:54:29 +0300

    new

--- EidelmanRoman/task04/task04.cpp
+++ EidelmanRoman/task04/task04.cpp
@@ -72,14 +72,17 @@ Matrix::Matrix(unsigned int input_rows, unsigned int input_cols) {
 
 Matrix::Matrix(const Matrix& X) {
     if (this != &X) {
-        delete[] data;
-        delete[] values;
         rows = X.rows;
         cols = X.cols;
         matrix_size = X.matrix_size;
         data = new double[matrix_size];
         values = new double *[rows];
-        memcpy(data, X.data,  matrix_size * sizeof(double));
+//        memcpy(data, X.data,  matrix_size * sizeof(double));
+        for (int row = 0; row < rows; ++row)
+            values[row] = data + row * cols;
+        for (int i = 0; i < matrix_size + rows; ++i) {
+            data[i] = X.data[i];
+        }
     }
 }
 
@@ -94,10 +97,16 @@ Matrix::~Matrix() {
     delete[] values;
 }
 
+void Matrix::swap(Matrix& X) {
+    std::swap(matrix_size, X.matrix_size);
+    std::swap(data, X.data);
+    std::swap(values, X.values);
+}
 
 Matrix& Matrix::operator=(const Matrix& X) {
     Matrix copy = X;
     swap(copy);
+    return *this;
 }
 
 Matrix& Matrix::operator+=(const Matrix& X) {
@@ -105,24 +114,24 @@ Matrix& Matrix::operator+=(const Matrix& X) {
         data[i] += X.data[i];
     return *this;
 }
-//
-//Matrix Matrix::operator+(const Matrix& X) {
-//    Matrix copy = *this;
-//    copy += X;
-//    return copy;
-//}
-//
-//Matrix& Matrix::operator-=(const Matrix& X) {
-//    for (int i = 0; i < matrix_size; ++i)
-//        data[i] -= X.data[i];
-//    return *this;
-//}
-//
-//Matrix Matrix::operator-(const Matrix& X) {
-//    Matrix copy = *this;
-//    copy -= X;
-//    return copy;
-//}
+
+Matrix Matrix::operator+(const Matrix& X) {
+    Matrix copy = *this;
+    copy += X;
+    return copy;
+}
+
+Matrix& Matrix::operator-=(const Matrix& X) {
+    for (int i = 0; i < matrix_size; ++i)
+        data[i] -= X.data[i];
+    return *this;
+}
+
+Matrix Matrix::operator-(const Matrix& X) {
+    Matrix copy = *this;
+    copy -= X;
+    return copy;
+}
 
 //Matrix& Matrix::operator*=(const Matrix& X) {
 //    Matrix zero(rows, cols, 0);
@@ -136,7 +145,7 @@ Matrix& Matrix::operator+=(const Matrix& X) {
 //    *this = zero;
 //    return *this;
 //}
-//
+
 //Matrix Matrix::operator*(const Matrix& X) {
 //    Matrix copy = *this;
 //    copy *= X;
@@ -165,12 +174,6 @@ Matrix& Matrix::operator+=(const Matrix& X) {
 //}
 
 
-void Matrix::swap(Matrix& X) {
-    std::swap(matrix_size, X.matrix_size);
-    std::swap(data, X.data);
-    std::swap(values, X.values);
-}
-
 void Matrix::print() {
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++)
@@ -186,10 +189,10 @@ int main() {
 
     Matrix A(n, m, 1);
     A.print();
-    Matrix B(n, m, 0);
-    B.print();
+    Matrix B;
     B = A;
     B.print();
+
 
 //    try {
 //        Matrix T(n, m);
