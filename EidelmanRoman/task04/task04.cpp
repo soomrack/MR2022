9509e337ddb13fbfb9bbf03201cdbf112e1b7b02
reiv27 <romaneidelman.offical@gmail.com>
2023-01-09 20:15:46 +0300

    new

--- EidelmanRoman/task04/task04.cpp
+++ EidelmanRoman/task04/task04.cpp
@@ -32,7 +32,7 @@ private:
     double** values = nullptr;
     double* data = nullptr;
 
-    void swap(Matrix& X);
+//    void swap(Matrix& X);
 public:
     Matrix() = default;
     Matrix(unsigned int input_rows, unsigned int input_cols);
@@ -72,14 +72,15 @@ Matrix::Matrix(unsigned int input_rows, unsigned int input_cols) {
 
 Matrix::Matrix(const Matrix& X) {
     if (this != &X) {
-        delete[] data;
-        delete[] values;
         rows = X.rows;
         cols = X.cols;
         matrix_size = X.matrix_size;
         data = new double[matrix_size];
         values = new double *[rows];
         memcpy(data, X.data,  matrix_size * sizeof(double));
+        for (int row = 0; row < rows; ++row)
+            values[row] = data + row * cols;
+
     }
 }
 
@@ -94,47 +95,49 @@ Matrix::~Matrix() {
     delete[] values;
 }
 
+//void Matrix::swap(Matrix& X) {
+//    std::swap(matrix_size, X.matrix_size);
+//    std::swap(data, X.data);
+//    std::swap(values, X.values);
+//}
 
 Matrix& Matrix::operator=(const Matrix& X) {
     if (this != &X) {
         rows = X.rows;
         cols = X.cols;
-        matrix_size = rows * cols;
-        delete[] values;
-        delete[] data;
+        matrix_size = X.matrix_size;
+        data = new double[matrix_size];
         values = new double *[rows];
-        data = new double [matrix_size];
-        memcpy(data, X.data, matrix_size * sizeof(double));
+        memcpy(data, X.data,  matrix_size * sizeof(double));
         for (int row = 0; row < rows; ++row)
             values[row] = data + row * cols;
     }
     return *this;
 }
 
-
 Matrix& Matrix::operator+=(const Matrix& X) {
     for (int i = 0; i < matrix_size; ++i)
         data[i] += X.data[i];
     return *this;
 }
-//
-//Matrix Matrix::operator+(const Matrix& X) {
-//    Matrix copy = *this;
-//    copy += X;
-//    return copy;
-//}
-//
-//Matrix& Matrix::operator-=(const Matrix& X) {
-//    for (int i = 0; i < matrix_size; ++i)
-//        data[i] -= X.data[i];
-//    return *this;
-//}
-//
-//Matrix Matrix::operator-(const Matrix& X) {
-//    Matrix copy = *this;
-//    copy -= X;
-//    return copy;
-//}
+
+Matrix Matrix::operator+(const Matrix& X) {
+    Matrix copy = *this;
+    copy += X;
+    return copy;
+}
+
+Matrix& Matrix::operator-=(const Matrix& X) {
+    for (int i = 0; i < matrix_size; ++i)
+        data[i] -= X.data[i];
+    return *this;
+}
+
+Matrix Matrix::operator-(const Matrix& X) {
+    Matrix copy = *this;
+    copy -= X;
+    return copy;
+}
 
 //Matrix& Matrix::operator*=(const Matrix& X) {
 //    Matrix zero(rows, cols, 0);
@@ -148,7 +151,7 @@ Matrix& Matrix::operator+=(const Matrix& X) {
 //    *this = zero;
 //    return *this;
 //}
-//
+
 //Matrix Matrix::operator*(const Matrix& X) {
 //    Matrix copy = *this;
 //    copy *= X;
@@ -177,12 +180,6 @@ Matrix& Matrix::operator+=(const Matrix& X) {
 //}
 
 
-void Matrix::swap(Matrix& X) {
-    std::swap(matrix_size, X.matrix_size);
-    std::swap(data, X.data);
-    std::swap(values, X.values);
-}
-
 void Matrix::print() {
     for (int row = 0; row < rows; row++) {
         for (int col = 0; col < cols; col++)
@@ -198,11 +195,10 @@ int main() {
 
     Matrix A(n, m, 1);
     A.print();
-    //Matrix B;
-    Matrix B(n, m, 0);
+    Matrix B;
+    B += A;
     B.print();
-    B = A;
-    B.print();
+
 
 //    try {
 //        Matrix T(n, m);
