eaa711c50ef6f02ca76ec8d73520820c0af2eedc
Igor Varlamov <113335326+Vilgiz@users.noreply.github.com>
2023-01-10 08:08:35 +0300

    Update Task_4.cpp

--- IgorVarlamov/Task_4.cpp
+++ IgorVarlamov/Task_4.cpp
@@ -17,13 +17,11 @@ public:
     }
 };
 
-
 Matrix_Exception NOTSQUARE("Error:the matrix must be square(should have nxn size)\n");
 Matrix_Exception NOTEQUAL("Error: the matrix should have a same size\n");
 Matrix_Exception MULTIPLYERROR("Error: first matrix cols not equal to second matrix row.\n");
 Matrix_Exception ZERODIVISION("Error: divide by zero\n");
 Matrix_Exception MEM_ERROR("Error: memory are not allocated\n");
-
 
 template<typename T>
 class Matrix 
@@ -36,20 +34,21 @@ protected:
 
 public:
 
-    static_assert(
+    static_assert
+        (
          is_same<double, T>::value ||
          is_same<float, T>::value ||
          is_same<long double, T>::value,
         "T must be int, double or float"
         );
-    const double EPS = 0.000001;
 
+    const double EPS = 0.000001;
 
     Matrix(unsigned int, unsigned int, T*);
     Matrix(unsigned int, unsigned int);
     Matrix();
-    Matrix(const Matrix&);
-    Matrix(Matrix&&) noexcept;
+    Matrix(const Matrix&);                  // Конструктор копирования матрицы
+    Matrix(Matrix&&) noexcept;              // Конструктор переноса матрицы
     ~Matrix();
 
 
@@ -70,13 +69,13 @@ public:
 
     bool operator!=(const Matrix<T>&);
     bool operator==(const Matrix<T>&);
+
     Matrix& operator=(const Matrix<T>&);
     Matrix& operator-=(const Matrix<T>&);
     Matrix& operator+=(const Matrix<T>&);
     Matrix& operator*=(const Matrix<T>&);
     Matrix& operator*=(const T);
     Matrix& operator/=(const T);
-
 
     Matrix<T> operator+ (const Matrix<T>&);
     Matrix<T> operator- (const Matrix<T>&);
@@ -149,7 +148,8 @@ Matrix<T>::Matrix(const Matrix& mat) : rows(mat.rows), cols(mat.cols)
 template <typename T>
 Matrix<T>::~Matrix()
 {
-    if (values != nullptr) {
+    if (values != nullptr) 
+    {
         delete[] values;
     }
 }
@@ -204,8 +204,10 @@ template <typename T>
 Matrix<T> Matrix<T>::set_transpose()
 {
     Matrix trans(this->cols, this->rows);
-    for (unsigned int row = 0; row < trans.rows; row++) {
-        for (unsigned int col = 0; col < trans.cols; col++) {
+    for (unsigned int row = 0; row < trans.rows; row++) 
+    {
+        for (unsigned int col = 0; col < trans.cols; col++) 
+        {
             trans.values[row * trans.cols + col] = this->values[col * trans.rows + row];
         }
     }
@@ -218,7 +220,8 @@ template <typename T>
 bool Matrix<T>::operator==(const Matrix<T>& A)
 {
     if (this->cols != A.cols || this->rows != A.rows) return false;
-    for (unsigned int idx = 0; idx < this->cols * this->rows; idx++) {
+    for (unsigned int idx = 0; idx < this->cols * this->rows; idx++) 
+    {
         if (abs(this->values[idx] - A.values[idx]) > EPS) return false;
     }
     return true;
@@ -249,7 +252,8 @@ template <typename T>
 Matrix<T>& Matrix<T>::operator+=(const Matrix<T>& matrix)
 {
     if (rows != matrix.rows || cols != matrix.cols) throw NOTEQUAL;
-    for (unsigned int index = 0; index < rows * cols; ++index) {
+    for (unsigned int index = 0; index < rows * cols; ++index) 
+    {
         this->values[index] += matrix.values[index];
     }
     return *this;
@@ -260,7 +264,8 @@ template <typename T>
 Matrix<T>& Matrix<T>::operator-=(const Matrix<T>& matrix)
 {
     if (rows != matrix.rows || cols != matrix.cols) throw NOTEQUAL;
-    for (unsigned int index = 0; index < rows * cols; ++index) {
+    for (unsigned int index = 0; index < rows * cols; ++index) 
+    {
         this->values[index] -= matrix.values[index];
     }
     return *this;
@@ -292,7 +297,8 @@ Matrix<T>& Matrix<T>::operator*=(const Matrix<T>& matrix)
 template <typename T>
 Matrix<T>& Matrix<T>::operator*=(const T k) 
 {
-    for (unsigned int idx = 0; idx < rows * cols; idx++) {
+    for (unsigned int idx = 0; idx < rows * cols; idx++) 
+    {
         this->values[idx] *= k;
     }
     return *this;
@@ -302,7 +308,8 @@ Matrix<T>& Matrix<T>::operator*=(const T k)
 template <typename T>
 Matrix<T>& Matrix<T>::operator/=(const T k) 
 {
-    for (unsigned int idx = 0; idx < rows * cols; idx++) {
+    for (unsigned int idx = 0; idx < rows * cols; idx++) 
+    {
         this->values[idx] /= k;
     }
     return *this;
@@ -377,9 +384,10 @@ Matrix<T> Matrix<T>::fill_from_array(T* array)
 
 
 template<typename T>
-Matrix<T> Matrix<T>::fill_random(unsigned int n )//Максимальное значение
+Matrix<T> Matrix<T>::fill_random(unsigned int n )                       //Максимальное значение
 {
-    for (unsigned int index = 0; index < cols * rows; index++) {
+    for (unsigned int index = 0; index < cols * rows; index++) 
+    {
         this->values[index] = T(rand() % n);
     }
     return (*this);
@@ -390,15 +398,23 @@ template <typename T>
 Matrix<T> Matrix<T>::minor(const Matrix matrix, const unsigned int size, const unsigned int row, const unsigned int col)
 {
     Matrix minor(size - 1, size - 1);
-    unsigned int shiftrow = 0; //Смещение индекса строки в матрице
-    unsigned int shiftcol; //Смещение индекса столбца в матрице
-    for (unsigned int rows = 0; rows < size - 1; rows++) {
-        //Пропустить row-ую строку
-        if (rows == row) { shiftrow = 1; } //Как только встретили строку,
-        //которую надо пропустить, делаем смещение для исходной матрицы
-        shiftcol = 0; //Обнулить смещение столбца
-        for (unsigned int cols = 0; cols < size - 1; cols++) {
-            if (cols == col) { shiftcol = 1; }
+    unsigned int shiftrow = 0;                                          //Смещение индекса строки в матрице
+    unsigned int shiftcol;                                              //Смещение индекса столбца в матрице
+    for (unsigned int rows = 0; rows < size - 1; rows++) 
+    {
+                                                                        //Пропустить row-ую строку
+        if (rows == row) 
+        { 
+            shiftrow = 1; 
+        }                                                               //Как только встретили строку,
+                                                                        //которую надо пропустить, делаем смещение для исходной матрицы
+        shiftcol = 0;                                                   //Обнулить смещение столбца
+        for (unsigned int cols = 0; cols < size - 1; cols++) 
+        {
+            if (cols == col) 
+            { 
+                shiftcol = 1; 
+            }
             minor.values[rows * (size - 1) + cols] = matrix.values[(rows + shiftrow)
                 * size + (cols + shiftcol)];
         }
@@ -417,7 +433,8 @@ double Matrix<T>::determinant(const Matrix matrix, const unsigned int size)
         return 0;
     if (size == 1)
         return matrix.values[0];
-    if (size == 2) {
+    if (size == 2) 
+    {
         return (matrix.values[0] * matrix.values[3] - matrix.values[2] * matrix.values[1]);
     }
     for (unsigned int idx = 0; idx < size; idx++) {
@@ -435,8 +452,10 @@ Matrix<T> Matrix<T>::reverse(const Matrix matrix, const unsigned int size)
     if (matrix.rows != matrix.cols) throw NOTSQUARE;
     Matrix reverse(matrix.rows, matrix.cols);
     int k = 1;
-    for (unsigned int row = 0; row < reverse.rows; row++) {
-        for (unsigned int col = 0; col < reverse.cols; col++) {
+    for (unsigned int row = 0; row < reverse.rows; row++) 
+    {
+        for (unsigned int col = 0; col < reverse.cols; col++) 
+        {
             reverse.values[row * size + col] = k * determinant(minor(matrix, size, row, col), size - 1);
             k = -k;
         }
@@ -449,7 +468,7 @@ Matrix<T> Matrix<T>::reverse(const Matrix matrix, const unsigned int size)
 
 
 template <typename T>
-Matrix<T> Matrix<T>::exponent(unsigned int n ) //Количество членов разложения)
+Matrix<T> Matrix<T>::exponent(unsigned int n ) //Количество членов разложения
 {
     if (rows != cols) throw NOTSQUARE;
     Matrix ex(rows, cols);
@@ -457,7 +476,8 @@ Matrix<T> Matrix<T>::exponent(unsigned int n ) //Количество члено
     temp.set_identity();
     ex.set_identity();
     double fact = 1.0;
-    for (unsigned int i = 1; i < n; i++) {
+    for (unsigned int i = 1; i < n; i++) 
+    {
         fact *= i;
         temp *= (*this);
         ex += temp / fact;
@@ -471,11 +491,13 @@ class Matrix_Memory : public Matrix<T1> {
 private:
     unsigned int memory = 0;
 public:
-    Matrix_Memory<T1>() {
+    Matrix_Memory<T1>() 
+    {
         memory += total_memory;
     }
 
-    void output() override {
+    void output() override 
+    {
          cout << "Memory allocated for class Matrix - " << memory << " byte" <<  endl;
     }
 
@@ -488,20 +510,21 @@ public:
 
 void test_add()
 {
-    double data1[6] = { 3, 1, 2,
-                        4, 5, 7 };
-    double data2[6] = { 8, 2, 1,
-                        -2, 2, 4 };
-    double ans_d[6] = { 11, 3, 3,
-                        2, 7, 11 };
+    double data1[6] = { 3, 1, 2, 4, 5, 7 };
+    double data2[6] = { 8, 2, 1, -2, 2, 4 };
+    double ans_d[6] = { 11, 3, 3, 2, 7, 11 };
+
     Matrix<double> matrix1 = Matrix<double>(2, 3).fill_from_array(data1);
     Matrix<double> matrix2 = Matrix<double>(2, 3).fill_from_array(data2);
     Matrix<double> test_add = Matrix<double>(2, 3).fill_from_array(ans_d);
-    if (test_add != matrix1 + matrix2) {
+
+    if (test_add != matrix1 + matrix2) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Addition test failed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "Addition test passed\n";
     }
@@ -511,15 +534,14 @@ void test_add()
 
 void test_sub()
 {
-    double data1[6] = { 3, 1, 2,
-                        4, 5, 7 };
-    double data2[6] = { 8, 2, 1,
-                        -2, 2, 4 };
-    double ans_d[6] = { -5, -1, 1,
-                        6, 3, 3 };
+    double data1[6] = { 3, 1, 2, 4, 5, 7 };
+    double data2[6] = { 8, 2, 1, -2, 2, 4 };
+    double ans_d[6] = { -5, -1, 1, 6, 3, 3 };
+
     Matrix<double> matrix1 = Matrix<double>(2, 3).fill_from_array(data1);
     Matrix<double> matrix2 = Matrix<double>(2, 3).fill_from_array(data2);
     Matrix<double> test_add = Matrix<double>(2, 3).fill_from_array(ans_d);
+
     if (test_add != matrix1 - matrix2) {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Substraction test failed\n";
@@ -534,18 +556,14 @@ void test_sub()
 
 void test_mult()
 {
-    double data1[9] = { 1, 7, 4,
-                        0, 9, 4,
-                        8, 8, 2 };
-    double data2[9] = { 4, 5, 5,
-                        1, 7, 1,
-                        1, 5, 2 };
-    double ans_d[9] = { 15, 74, 20,
-                        13, 83, 17,
-                        42, 106, 52 };
+    double data1[9] = { 1, 7, 4, 0, 9, 4, 8, 8, 2 };
+    double data2[9] = { 4, 5, 5, 1, 7, 1, 1, 5, 2 };
+    double ans_d[9] = { 15, 74, 20, 13, 83, 17, 42, 106, 52 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 3).fill_from_array(data1);
     Matrix<double> matrix2 = Matrix<double>(3, 3).fill_from_array(data2);
     Matrix<double> test_add = Matrix<double>(3, 3).fill_from_array(ans_d);
+
     if (test_add != matrix1 * matrix2) {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Multiplication test failed\n";
@@ -562,8 +580,10 @@ void test_mult_k()
 {
     double data1[3] = { 1, 7, 4 };
     double ans_d[3] = { 3, 21, 12 };
+
     Matrix<double> matrix1 = Matrix<double>(1, 3).fill_from_array(data1);
     Matrix<double> test_add = Matrix<double>(1, 3).fill_from_array(ans_d);
+
     if (test_add != matrix1 * 3) {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Multiplication test failed\n";
@@ -576,16 +596,21 @@ void test_mult_k()
 }
 
 
-void test_div_k() {
+void test_div_k() 
+{
     double data1[3] = { 6, 21, 18 };
     double ans_d[3] = { 2, 7, 6 };
+
     Matrix<double> matrix1 = Matrix<double>(1, 3).fill_from_array(data1);
     Matrix<double> test_div = Matrix<double>(1, 3).fill_from_array(ans_d);
-    if (test_div != matrix1 / 3) {
+
+    if (test_div != matrix1 / 3) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Division test failed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "Division test passed\n";
     }
@@ -595,16 +620,18 @@ void test_div_k() {
 
 void test_det()
 {
-    double data1[9] = { 1, 7, 4,
-                        0, 9, 4,
-                        8, 8, 2 };
+    double data1[9] = { 1, 7, 4, 0, 9, 4, 8, 8, 2 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 3).fill_from_array(data1);
+
     double determinant = matrix1.determinant(matrix1, matrix1.getrow());
-    if (determinant != -78.0) {
+    if (determinant != -78.0) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Determinant test failed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "Determinant test passed\n";
     }
@@ -614,20 +641,20 @@ void test_det()
 
 void test_rev() 
 {
-    double data1[9] = { 1, 7, 4,
-                        0, 9, 4,
-                        8, 8, 2 };
-    double ans[9] = { 0.179487,  -0.230769, 0.102564,
-                      -0.410256,  0.384615, 0.0512821,
-                      0.923077,   -0.615385, -0.115385 };
+    double data1[9] = { 1, 7, 4, 0, 9, 4, 8, 8, 2 };
+    double ans[9] = { 0.179487,  -0.230769, 0.102564, -0.410256,  0.384615, 0.0512821, 0.923077,   -0.615385, -0.115385 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 3).fill_from_array(data1);
     Matrix<double> test_rev = Matrix<double>(3, 3).fill_from_array(ans);
     Matrix<double> reverse = matrix1.reverse(matrix1, matrix1.getrow());
-    if (reverse == test_rev) {
+
+    if (reverse == test_rev) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "Reverse test passed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Reverse test failed\n";
     }
@@ -639,13 +666,17 @@ void test_transpose()
 {
     double data1[3] = { 1, 7, 4 };
     double ans[3] = { 1,7,4 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 1).fill_from_array(data1);
     Matrix<double> test_trans = Matrix<double>(1, 3).fill_from_array(ans);
-    if (matrix1.set_transpose() == test_trans) {
+
+    if (matrix1.set_transpose() == test_trans) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "Trasposition test passed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "Transposition test failed\n";
     }
@@ -655,19 +686,19 @@ void test_transpose()
 
 void test_set_identity()
 {
-    double data1[9] = { 1, 7, 4,
-                        0, 9, 4,
-                        8, 8, 2 };
-    double ans[9] = { 1, 0, 0,
-                      0, 1, 0,
-                      0, 0, 1 };
+    double data1[9] = { 1, 7, 4, 0, 9, 4, 8, 8, 2 };
+    double ans[9] = { 1, 0, 0, 0, 1, 0, 0, 0, 1 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 3).fill_from_array(data1);
     Matrix<double> test_identity = Matrix<double>(3, 3).fill_from_array(ans);
-    if (matrix1.set_identity() == test_identity) {
+
+    if (matrix1.set_identity() == test_identity) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "SetIdentity test passed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "SetIdentity test failed\n";
     }
@@ -678,20 +709,25 @@ void test_set_zero()
 {
     double data1[3] = { 1, 7, 4 };
     double ans[3] = { 0,0,0 };
+
     Matrix<double> matrix1 = Matrix<double>(3, 1).fill_from_array(data1);
     Matrix<double> test_zero = Matrix<double>(3, 1).fill_from_array(ans);
-    if (matrix1.set_zero() == test_zero) {
+
+    if (matrix1.set_zero() == test_zero) 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
          cout << "SetZero test passed\n";
     }
-    else {
+    else 
+    {
         SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
          cout << "SetZero test failed\n";
     }
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
 }
 
-void test() {
+void test() 
+{
     test_add();
     test_sub();
     test_div_k();
@@ -705,19 +741,22 @@ void test() {
 }
 
 
-int main() {
+int main() 
+{
     
     try
     {
         Matrix<double> A(3, 3);
     }
-    catch ( bad_alloc& e) {
+    catch ( bad_alloc& e) 
+    {
          cerr << "Caught: " << e.what() <<  endl;
         exit(101);
     }
 
     Matrix<double> A(3, 3);
     A.fill_random(10);
+
     Matrix<double> B(3, 3);
     B.fill_random(10);
 
@@ -727,28 +766,35 @@ int main() {
      cout << "First Matrix\n" << A;
      cout << "Second Matrix\n" << B;
 
-    try {
+    try 
+    {
          cout << "Addiction of two matrix\n" << A + B;
          cout << "Subtraction of two matrix\n" << A - B;
     }
-    catch (const Matrix_Exception &e) {
+    catch (const Matrix_Exception &e) 
+    {
          cerr << "Caught: " << e.what() <<  endl;
     }
-    try {
+    try 
+    {
          cout << "Multiplication of two matrix\n" << A * B;
     }
-    catch (const Matrix_Exception &e) {
+    catch (const Matrix_Exception &e) 
+    {
          cerr << "Caught: " << e.what() <<  endl;
     }
-    try {
+    try 
+    {
          cout << "Determinant\n" << A.determinant(A, A.getrow()) <<  endl;
          cout << "Reverse Matrix\n" << A.reverse(A, A.getrow()) <<  endl;
          cout << "Exponent Matrix\n" << A.exponent(30);
     }
-    catch (Matrix_Exception &e) {
+    catch (Matrix_Exception &e) 
+    {
          cerr << "Caught: " << e.what() <<  endl;
     }
     Matrix_Memory<double> mem;
     mem.output();
+
     test();
 }
