52005afb508cb6c8e6cd2c7094ec9179ebb04029
KonevIgor <konik562@yandex.ru>
2023-01-10 09:30:45 +0300

    Commit for exam

--- KonevIgor/2.task_two/main.c
+++ KonevIgor/2.task_two/main.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <malloc.h>
 #include "matrix.h"
 
 int main() {
-    unsigned int operation_choice = 6;
+    unsigned int operation_choice = 4;
     // Выбор арифметической операции, производимой с матрицами, где
     // 1 - Операция сложения матриц
     // 2 - Операция вычитания матриц
     // 3 - Операция умножения матриц
-    // 4 - Операция вычисления определителя матрицы методом Гаусса
-    // 5 - Операция вычисления определителя с помощью миноров
-    // 6 - Операция вычисления обратной матрицы
-    // 7 - Операция взятия матричной экспоненты
+    // 4 - Операция вычисления определителя с помощью миноров
+    // 5 - Операция вычисления обратной матрицы
+    // 6 - Операция взятия матричной экспоненты
     Matrix first;
     Matrix second;
     Matrix result;
@@ -19,23 +19,33 @@ int main() {
     print_matrix(first);
     printf("\nMatrix B:\n");
     print_matrix(second);
+    printf("\n");
     switch (operation_choice) {
         case 1: {
             printf("\nA + B:\n");
             print_matrix(matrix_add(&result, first, second));
-            is_ans_correct_add(result, first, second);
+            if (is_ans_correct_add(result, first, second)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             break;
         }
         case 2: {
             printf("\nA - B:\n");
             print_matrix(matrix_sub(&result, first, second));
-            is_ans_correct_sub(result, first, second);
+            if (is_ans_correct_sub(result, first, second)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             break;
         }
         case 3: {
             printf("\nA * B:\n");
             print_matrix(matrix_mult(&result, first, second));
-            is_ans_correct_mult(result, first, second);
+            if (is_ans_correct_mult(result, first, second)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             break;
         }
         case 4: {
@@ -43,22 +53,34 @@ int main() {
             printf("\n|B| = %f\n", matrix_det_gauss(&result, second));
             break;
         }
-        case 6: {
+        case 5: {
             printf("\nA^(-1):\n");
             print_matrix(matrix_inv(&result, first));
-            is_ans_correct_inv(result, first);
+            if (is_ans_correct_inv(result, first)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             printf("\nB^(-1):\n");
             print_matrix(matrix_inv(&result, second));
-            is_ans_correct_inv(result, second);
+            if (is_ans_correct_inv(result, second)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             break;
         }
-        case 7: {
+        case 6: {
             printf("\nexp(A)\n");
             print_matrix(matrix_exp(&result, first));
-            is_ans_correct_exp(result, first);
+            if (is_ans_correct_exp(result, first)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             printf("\nexp(B)\n");
             print_matrix(matrix_exp(&result, second));
-            is_ans_correct_exp(result, second);
+            if (is_ans_correct_exp(result, second)) {
+                printf("The answer is correct.\n");
+            } else
+                printf("The answer is incorrect.\n");
             break;
         }
         default: {
--- KonevIgor/task_three/matrixException.cpp
+++
@@ -1,5 +0,0 @@
-#include "matrixException.hpp"
-
-MatrixException::MatrixException(const std::string& message):
-  std::logic_error(message)
-{}
