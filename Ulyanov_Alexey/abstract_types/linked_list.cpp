64a978dae42552c23512844ed7453dcbef1ffa0e
Ульянов Алексей <ulalex.2002.2@mail.ru>
2023-02-19 15:27:18 +0300

    double linked list completely changed

--- Ulyanov_Alexey/abstract_types/linked_list.cpp
+++ Ulyanov_Alexey/abstract_types/linked_list.cpp
@@ -2,88 +2,92 @@
 
 const double COMPARATION_CONST = 0.0001;
 
-class Linked_list {
+
+class item {
+
+protected:
+    item *next;   // next последнего указывает на начало
+    item *previous;
 
 public:
+    double value;
 
-    class element {
+    item();
+    item(const double x);
+    item(const item& x);
+    item(item&& x);
+    ~item();
 
-    protected:
-        element * next;   // next последнего указывает на начало
-        element * previous;
+    item& operator=(const double& x);
+    item& operator=(const item& x);
+
+    void setNext(item* x);
+    void setPrevios(item* x);
+
+    item* getNext();
+    item* getPrevios();
+
+};
+
+const item NULL_ITEM = item(0.0);
 
 
-    public:
-        double value;
-
-        element();
-        element(const double x);
-        element(const element& x);
-        element(element&& x);
-        ~element();
-
-        element& operator=(const double& x);
-        element& operator=(const element& x);
-
-        void setNext(element* x);
-        void setPrevios(element* x);
-
-        element* getNext();
-        element* getPrevios();
-
-    };
+class Linked_list {
 
 protected:
 
     unsigned int size;
-    element* data;
+    item *head;
 
 public:
 
     Linked_list();
-    Linked_list(element head);
+    Linked_list(double head_vle);
     Linked_list(const Linked_list& x);
     Linked_list(Linked_list&& x);
     ~Linked_list();
 
     Linked_list& operator=(const Linked_list& x);
 
-    void add_tail(element elm);
+    void add_head(double vle);
+    void add_tail(double vle);
+    void add(double vle, unsigned int position = 0);
+    void add_next_to(double sup_vle, double new_vle);  // от support_value
+    void add_previous_to(double sup_vle, double new_vle);
+
     unsigned int contains(double vle);
     void del(double vle);
-    element get(double vle);
-    void add(element elm, unsigned int position = 0);
+    item get(double vle);
 
+    item get_head();
     void output(bool show_data = false);
 
 };
 
 
-const Linked_list::element UNDEFINDED_ELEMENT = Linked_list::element(0.0);
 
-
-Linked_list::element::element() {
+item::item() {  // is undefinded item;
     value = 0.0;
     next = nullptr;
     previous = nullptr;
 }
 
 
-Linked_list::element::element(const double x) {
+item::item(const double x) {
     value = x;
-    next = this;
-    previous = this;
+    next = nullptr;
+    previous = nullptr;
 }
 
 
-Linked_list::element::element(const Linked_list::element &x) {
+item::item(const item &x) {
     value = x.value;
     next = x.next;
     previous = x.previous;
 }
 
 
-Linked_list::element::element(Linked_list::element &&x) {
+item::item(item &&x) {
     value = x.value;
     next = x.next;
     previous = x.previous;
@@ -94,122 +98,179 @@ Linked_list::element::element(Linked_list::element &&x) {
 }
 
 
-Linked_list::element::~element() {
+item::~item() {
     next = nullptr;
     previous = nullptr;
 }
 
 
-Linked_list::element& Linked_list::element::operator=(const double &x) {
+item& item::operator=(const double &x) {
     value = x;
-    next = nullptr;
-    previous = nullptr;
+    next = this;
+    previous = this;
 }
 
 
-Linked_list::element& Linked_list::element::operator=(const element &x) {
+item& item::operator=(const item &x) {
     value = x.value;
     next = x.next;
     previous = x.previous;
 }
 
 
-Linked_list::element* Linked_list::element::getNext() {
+item* item::getNext() {
     return next;
 }
 
 
-Linked_list::element* Linked_list::element::getPrevios() {
+item* item::getPrevios() {
     return previous;
 }
 
 
-void Linked_list::element::setNext(Linked_list::element *x) {
+void item::setNext(item *x) {
     next = x;
 }
 
 
-void Linked_list::element::setPrevios(Linked_list::element *x) {
+void item::setPrevios(item *x) {
     previous = x;
 }
 
 
 Linked_list::Linked_list() {
+    head = nullptr;
     size = 0;
-    data = nullptr;
 }
 
 
-Linked_list::Linked_list(element head) {
-    size = 1;
-    data = new element[size];
-    data[0] = head;
-}
-
-
-Linked_list::Linked_list(const Linked_list& x) {
-    size = x.size;
-    data = new element[size];
-
-    for (unsigned int idx = 0; idx < x.size; idx++){
-        data[idx] = x.data[idx];
-    }
-}
-
-
-Linked_list::Linked_list(Linked_list &&x) {
-    size = x.size;
-    data = x.data;
-
-    x.size = 0;
-    x.data = nullptr;
+Linked_list::Linked_list(double head_value) {
+    size = 0;
+    add_head(head_value);
 }
 
 
 Linked_list::~Linked_list() {
-    delete[] data;
+    head = nullptr;
 }
 
 
 Linked_list &Linked_list::operator=(const Linked_list &x) {
-    if (this != &x){
-        if (!data)
-            delete[] data;
-
-        size = x.size;
-
-        this->data = new element[size];
-        for (unsigned int idx = 0; idx < size; idx++){
-            data[idx] = x.data[idx];
-        }
-    }
-
-    return *this;
+    head = x.head;
+    size = x.size;
 }
 
 
-void Linked_list::add_tail(Linked_list::element elm) {
-    Linked_list::element* temp = new Linked_list::element[size + 1];
-    for (unsigned int idx = 0; idx < size; idx++){
-        temp[idx] = data[idx];
+void Linked_list::add_head(double vle) {
+    item* elm = new item(vle);
+    if (size == 0){
+        elm->setNext(elm);
+        elm->setPrevios(elm);
+        head = elm;
+    } else {
+        head->getPrevios()->setNext(elm);
+        elm->setNext(head);
+        elm->setPrevios(head->getPrevios());
+        head->setPrevios(elm);
+        head = elm;
     }
-    delete[] data;
-    data = temp;
-    data[size] = elm;
+    size++;
+}
 
-    data[size - 1].setNext(&data[size]);
-    data[size].setPrevios(&data[size - 1]);
 
-    data[size].setNext(&data[0]);
-    data[0].setPrevios(&data[size++]);
+void Linked_list::add_tail(double vle) {
+    item* elm = new item(vle);
+    if (size == 0){
+        elm->setNext(elm);
+        elm->setPrevios(elm);
+        head = elm;
+    } else {
+        elm->setPrevios(head->getPrevios());
+        elm->setNext(head);
+        head->getPrevios()->setNext(elm);
+        head->setPrevios(elm);
+    }
+    size++;
+}
+
+
+void Linked_list::add(double vle, unsigned int position) {
+    if (position > size) return;
+    if (position == 0){
+        add_head(vle);
+        return;
+    }
+    if (position == size){
+        add_tail(vle);
+        return;
+    }
+
+    item* elm = head;
+    for (unsigned int idx = 0; idx < position; idx++){
+        elm = elm->getNext();
+    }
+    item* new_item = new item(vle);
+
+    elm->getPrevios()->setNext(new_item);
+    new_item->setPrevios(elm->getPrevios());
+    new_item->setNext(elm);
+    elm->setPrevios(new_item);
+
+    size++;
+}
+
+
+void Linked_list::add_next_to(double sup_vle, double new_vle) {
+    unsigned int point = contains(sup_vle);
+    if (point == 0) return;
+
+    item* elm = head;
+    for (unsigned int idx = 1; idx < point; idx++){
+        elm = elm->getNext();
+    }
+
+    item* new_elm = new item(new_vle);
+    elm->getNext()->setPrevios(new_elm);
+    new_elm->setNext(elm->getNext());
+    elm->setNext(new_elm);
+    new_elm->setPrevios(elm);
+
+    size++;
+}
+
+
+void Linked_list::add_previous_to(double sup_vle, double new_vle) {
+    unsigned int point = contains(sup_vle);
+    if (point == 0) return;
+
+    if (point == 1){
+        add_head(new_vle);
+        return;
+    }
+    item* elm = head;
+    for (unsigned int idx = 1; idx < point - 1; idx++){
+        elm = elm->getNext();
+    }
+
+    item* new_elm = new item(new_vle);
+    elm->getNext()->setPrevios(new_elm);
+    new_elm->setNext(elm->getNext());
+    elm->setNext(new_elm);
+    new_elm->setPrevios(elm);
+
+    size++;
 }
 
 
 unsigned int Linked_list::contains(double vle) {
+    if (size == 0) return 0;
+
+    item* elm = head;
     for (unsigned int idx = 0; idx < size; idx++){
-        if (abs(data[idx].value - vle) < COMPARATION_CONST){
+        if (abs(elm->value - vle) < COMPARATION_CONST){
             return idx + 1;
         }
+        elm = elm->getNext();
     }
     return 0;
 }
@@ -219,70 +280,87 @@ void Linked_list::del(double vle) {
     unsigned int point = contains(vle);
     if (point == 0) return;
 
-    if (point == size){
-        data[point - 2].setNext(&data[0]);
-        data[0].setPrevios(&data[point - 2]);
-    } else if (point == 1){
-        data[size - 1].setNext(&data[point]);
-        data[point].setPrevios(&data[size - 1]);
-    } else {
-        data[point - 2].setNext(&data[point]);
-        data[point].setPrevios(&data[point - 2]);
+    item* elm = head;
+    for (unsigned int idx = 1; idx < point; idx++){
+        elm = elm->getNext();
     }
 
-    element *temp = new element [size - 1];
-    for (unsigned int idx = 0; idx < point - 1; idx++){
-        temp[idx] = data[idx];
-    }
-    for (unsigned int idx = point; idx < size; idx++){
-        temp[idx - 1] = data[idx];
-    }
-    //delete[] data;
-    data = temp;
+    elm->getPrevios()->setNext(elm->getNext());
+    elm->getNext()->setPrevios(elm->getPrevios());
+    if (point == 1) head = elm->getNext();
     size--;
 }
 
 
-Linked_list::element Linked_list::get(double vle) {
-    unsigned int point = contains(vle);
-    if (point == 0) return UNDEFINDED_ELEMENT;
-    return data[point - 1];
+item Linked_list::get(double vle) {
+    if (size == 0) return NULL_ITEM;
+
+    item* elm = head;
+    for (unsigned int idx = 0; idx < size; idx++){
+        if (abs(elm->value - vle) < COMPARATION_CONST){
+            return *elm;
+        }
+        elm = elm->getNext();
+    }
+    return NULL_ITEM;
 }
 
 
-void Linked_list::add(Linked_list::element elm, unsigned int position) {
-    if (position >= size) return;
-
+item Linked_list::get_head() {
+    return *head;
 }
 
 
 void Linked_list::output(bool show_data) {
+    if (show_data){
+        std::cout << "The chain's length: " << size << "\n";
+    }
+    if (size == 0) return;
+
+    item* current = head;
     for (unsigned int idx = 0; idx < size; idx++){
-        std::cout << data[idx].value << " ";
+        std::cout << current->value << "  ";
         if (show_data){
-            std::cout << "  his previous is " << data[idx].getPrevios()->value <<
-                         " and his next is " << data[idx].getNext()->value << "\n";
+            std::cout << "his previous is " << current->getPrevios()->value << "  and " <<
+                      "his next is " << current->getNext()->value << "\n";
         }
+        current = current->getNext();
     }
     std::cout << "\n";
 }
 
 
 int main() {
-    Linked_list A;
-    Linked_list B(Linked_list::element(5.1));
+
+    Linked_list A = Linked_list();
+    A.add_head(10.1);
+    A.output(true);
+    A.del(10.1);
+    A.output(true);
+
+
+    Linked_list B = Linked_list(0.1);
     B.output(true);
 
-    Linked_list::element x = Linked_list::element(6.5);
-    B.add_tail(x);
-    x = 6.7;
-    B.add_tail(x);
-
+    B.add_head(1.2);
     B.output(true);
 
-    B.del(6.7);
-
+    B.add_tail(2.3);
     B.output(true);
+
+    B.add(9.9, 2);
+    B.output(true);
+
+    B.del(1.2);
+    B.output(true);
+
+    B.add_next_to(2.3, 3.4);
+    B.output(true);
+
+    B.add_previous_to(0.1, 4.5);
+    B.output(true);
+
+    std::cout << B.get(10.9).value << "\n";
 
     return 0;
 }
