db379d24b86c815a3072fd0a78e45f00e8ae9175
reiv27 <romaneidelman.offical@gmail.com>
2023-02-26 18:18:13 +0300

    Class List was added.

--- EidelmanRoman/semester6/List/List.h
+++ EidelmanRoman/semester6/List/List.h
@@ -2,8 +2,148 @@
 #define MR2022_LIST_H
 
 
+class List {
+private:
+
+    class Node {
+    public:
+        Node* next;
+        double data;
+
+        Node(double data = 0.0, Node* next = nullptr) {
+            this->data = data;
+            this->next = next;
+        }
+    };
+    unsigned int Size;
+    Node* head;
+
+public:
+
+    List();
+    ~List();
+
+    void push_back(double data);
+    void push_front(double data);
+    void push_to(double data, int index);
+    void pop_front();
+    void pop_from(int index);
+    void pop_back();
+    void clear();
+    unsigned int getSize() {
+        return Size;
+    };
+
+    double& operator[](const int index);
 
 
+};
+
+
+List::List() {
+    Size = 0;
+    head = nullptr;
+};
+
+
+List::~List() {
+    clear();
+}
+
+
+void List::pop_front() {
+
+    Node* temp = head;
+
+    head = head->next;
+
+    delete temp;
+
+    --Size;
+}
+
+
+void List::push_back(double data) {
+
+    if (head == nullptr)
+        head = new Node(data);
+    else {
+        Node* current = this->head;
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+        current->next = new Node(data);
+    }
+
+    ++Size;
+}
+
+
+void List::push_front(double data) {
+    head = new Node(data, head);
+    ++Size;
+}
+
+
+void List::push_to(double data, int index) {
+    if (index == 0) {
+        push_front(data);
+    }
+    else {
+        Node* previous = this->head;
+
+        for (int i = 0; i < index - 1; ++i)
+            previous = previous->next;
+
+        Node* newNode = new Node(data, previous->next);
+        previous->next = newNode;
+        ++Size;
+    }
+}
+
+void List::pop_from(int index) {
+    if (index == 0) {
+        pop_front();
+    }
+    else {
+        Node* previous = this->head;
+
+        for (int i = 0; i < index - 1; ++i)
+            previous = previous->next;
+
+        Node* fromDelete = previous->next;
+        previous->next = fromDelete->next;
+        --Size;
+    }
+}
+
+
+void List::pop_back() {
+    pop_from(Size - 1);
+}
+
+
+void List::clear() {
+    while (Size) {
+        pop_front();
+    }
+}
+
+
+double& List::operator[](const int index) {
+
+    int counter = 0;
+
+    Node* current = this->head;
+
+    while (current != nullptr) {
+        if (counter == index) {
+            return current->data;
+        }
+        current = current->next;
+        ++counter;
+    }
+}
 
 
 #endif
--- EidelmanRoman/semester6/List/main.cpp
+++ EidelmanRoman/semester6/List/main.cpp
@@ -1,3 +1,13 @@
-//
-// Created by user on 26.02.23.
-//
+#include <iostream>
+#include "List.h"
+
+
+int main() {
+    List lst;
+    lst.push_back(5);
+    lst.push_back(6);
+    lst.push_back(8);
+
+    std::cout << lst.getSize() << "\n";
+    std::cout << lst[2] << "\n";
+}
