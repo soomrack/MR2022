b9b65999fd80d8125599cde0450e339122c2aa85
AntonPolyTech <COOLgay123@mail.ru>
2023-02-27 13:04:37 +0300

    -

--- Tyurin_Anton/List/List.cpp
+++ Tyurin_Anton/List/List.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 template<typename T>
 class List;
 
@@ -6,57 +7,78 @@ template<typename T>
 class Node {
     friend class List<T>;
 protected:
-    unsigned int adress;
-    T *data;
+    Node<T> *next;
+    T data;
 public:
-    Node(unsigned int newadress, T *newdata);
+    Node(T _data) : data(_data), next(nullptr){}
 };
-
-template<typename T>
-Node<T>::Node(unsigned int newadress, T *newdata){
-    adress = newadress;
-    data = new T[adress];
-    data = newdata;
-}
 
 template<typename T>
 class List {
 protected:
-    unsigned int size;
-    Node<T> first_el;
-    Node<T> last_el;
+    Node<T> *first_el;
+    Node<T> *last_el;
 public:
-    //void print(const List<T> &List);
-    List();
-    void push_tale(unsigned int nadress, T ndata);
+    List() : first_el(nullptr), last_el(nullptr) {}
+    bool is_empty();
+    void push_tale(T _data);
+    void print();
+    void push_head(T _data);
 };
 
-template<typename T>
-List<T>::List(){
-    Node<T> Node(0,0);
-    size = 1;
-    first_el = Node;
-    last_el = Node;
+template <typename T>
+bool List<T>::is_empty() {
+    return first_el == nullptr;
 }
 
-/* template<typename T>
-void List<T>::print(const List<T> &List){
-    for (unsigned int idx = 0; idx < List.size; idx++){
-        std::cout << List.data[idx] << ' ';
+template <typename T>
+void List<T>::push_tale(T _data){
+    Node<T> *local = new Node<T>(_data);
+    if (is_empty()) {
+        first_el = local;
+        last_el = local;
+        return;
+    }
+    last_el->next = local;
+    last_el = local;
+}
+
+template<typename T>
+void List<T>::push_head(T _data){
+    Node<T> *local = new Node<T>(_data);
+    if (is_empty()) {
+        first_el = local;
+        last_el = local;
+        return;
+    }
+    local->next = first_el;
+    first_el = local;
+}
+
+
+template<typename T>
+void List<T>::print() {
+    if (is_empty()) {return;}
+    Node<T> *local = first_el;
+    while (local) {
+        std::cout << local->data << " ";
+        local = local->next;
     }
     std::cout << std::endl;
-} */
-
-template<typename T>
-void List<T>::push_tale(unsigned int nadress, T ndata){
-    Node<T> Tale;
-    Tale.adress = nadress;
-    Tale.data = ndata;
-    last_el = Tale;
 }
 
+
+
 int main(){
-    List<double> List();
-    List.push_tale(0,12);
+    List<std::string> List;
+    List.print();
+
+
+    List.push_tale("ending");
+    List.print();
+
+    List.push_head("begining");
+    List.print();
+
     return 0;
 }
