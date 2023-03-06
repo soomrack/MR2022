34c2c310bb5a58ce505aae834c8b96e50e90aa2a
mvofus <m.v.of@ya.ru>
2023-03-06 14:09:28 +0300

    Added some adjustments

--- Malykhin_Vladislav/ADT/Dynamic_array/DArray.h
+++ Malykhin_Vladislav/ADT/Dynamic_array/DArray.h
@@ -2,23 +2,28 @@
 #define MR2022_DARRAY_H
 
 #include <iostream>
-#include <vector>
-#include "DarrayException.h"
+#include <list>
+#include <iterator>
+
+/* добавить кеш (не занятую память для последующего расширения, как новый аргумент класса),
+ заменить вектор на какое-нибудь заполнение из списка (зачем нужен наш класс, если есть вектор?), CHECK
+ удалить не нужный buffer, копировать напрямую в "нововыделенную" память, CHECK
+ можно перегрузить квадратные скобочки,
+ можно выкинуть исключения CHECK
+ */
 
 
 template<typename T>
 class DArray {
 protected:
     unsigned int size;
+    unsigned int cache_size = 0;
     T *data;
 public:
-    explicit DArray(std::vector<T> in_vector); // заполнение массива значениями из вектора при создании
+    explicit DArray(std::list<T> in_list, int new_cache_size = 5); // заполнение массива значениями из массива при создании
     ~DArray() noexcept;
-    DArray(const DArray<T> &other) ;
-    DArray(DArray<T> &&other) noexcept;
-
     unsigned int get_size() const;
-    T get_data(unsigned int id) const;
+    T get(unsigned int id) const;
     void set(unsigned int id, T element);
     void resize(unsigned int new_size);
     void print() const;
--- Malykhin_Vladislav/ADT/Dynamic_array/DArrayTest.cpp
+++ Malykhin_Vladislav/ADT/Dynamic_array/DArrayTest.cpp
@@ -1,25 +1,21 @@
 #include "DArrayTest.h"
 
 
-inline void print_message(const std::string& text, const DArrayException& Exception_object) {
-    std::cout << text << ":" << std::endl << Exception_object.get_message() << std::endl << std::endl;
-}
-
 
 template<typename T>
 void DArrayTest<T>::set_test(){
     std::cout << "Testing set method:\n";
     int flag_counter = 0;
-    DArray<T> dArray1(DArrayTest::test_vector);
-    std::cout << "Test value\tTrue value\tflag \n";
+    DArray<T> dArray1(DArrayTest::test_list);
+    std::cout << "Test value\t True value\tflag \n";
     for (int id = 0; id < dArray1.get_size(); ++id) {//цикл увеличивает все значения массива на 1 и проверяет соответствие
-        dArray1.set(id, dArray1.get_data(id) + 1);
+        dArray1.set(id, dArray1.get(id) + 1);
         int flag = 0;
-        if(dArray1.get_data(id) != DArrayTest::true_vector0[id]) {
+        if(dArray1.get(id) != DArrayTest::true_array0[id]) {
             flag = 1;
             ++flag_counter;
         }
-        std::cout << dArray1.get_data(id) << "\t \t " << true_vector0[id] << "\t\t"<< flag << "\n";
+        std::cout << dArray1.get(id) << "\t \t " << true_array0[id] << "\t\t"<< flag << "\n";
     }
     std::cout << "Flag counter value = " << flag_counter << "\n\n";
 }
@@ -29,32 +25,31 @@ template<typename T>
 void DArrayTest<T>::resize_test(){
     std::cout << "Testing resize method (size decreased):\n";
     int flag_counter = 0;
-    DArray<T> dArray1(DArrayTest::test_vector); // проверка уменьшения размера массива
+    DArray<T> dArray1(DArrayTest::test_list); // проверка уменьшения размера массива
     dArray1.resize(dArray1.get_size() - 1);
-    std::cout << "Test value \t True value \t flag \n";
+    std::cout << "Test value \t True value \tflag \n";
     for (int id = 0; id < dArray1.get_size(); ++id) {//проверяет соответствие значений массива
         int flag = 0;
-        if(dArray1.get_data(id) != DArrayTest::true_vector1[id]) {
+        if(dArray1.get(id) != DArrayTest::true_array1[id]) {
             flag = 1;
             ++flag_counter;
         }
-        std::cout << dArray1.get_data(id) << "\t \t " << true_vector1[id] << "\t\t"<< flag << "\n";
+        std::cout << dArray1.get(id) << "\t \t " << true_array1[id] << "\t\t"<< flag << "\n";
     }
     std::cout << "Flag counter value = " << flag_counter << "\n\n";
-
     std::cout << "Testing resize method (size increased):\n";
     flag_counter = 0;
-    DArray<T> dArray2(DArrayTest::test_vector); // проверка увеличения размера массива
+    DArray<T> dArray2(DArrayTest::test_list); // проверка увеличения размера массива
     dArray2.resize(dArray2.get_size() + 1);
     dArray2.set(dArray2.get_size() - 1, 5);
-    std::cout << "Test value \t True value \t flag \n";
+    std::cout << "Test value \t True value \tflag \n";
     for (int id = 0; id < dArray2.get_size(); ++id) {//проверяет соответствие значений массива
         int flag = 0;
-        if(dArray2.get_data(id) != DArrayTest::true_vector2[id]) {
+        if(dArray2.get(id) != DArrayTest::true_array2[id]) {
             flag = 1;
             ++flag_counter;
         }
-        std::cout << dArray2.get_data(id) << "\t \t " << true_vector2[id] << "\t\t"<< flag << "\n";
+        std::cout << dArray2.get(id) << "\t \t " << true_array2[id] << "\t\t"<< flag << "\n";
     }
     std::cout << "Flag counter value = " << flag_counter << "\n\n";
 }
@@ -65,6 +60,7 @@ void DArrayTest<T>::full_test() {
     DArrayTest<T>::set_test();
     DArrayTest<T>::resize_test();
 }
+
 
 template class DArrayTest<int>;
 template class DArrayTest<double>;
