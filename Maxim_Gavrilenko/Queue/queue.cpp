857ff409f2d8fc513d83b2336734bdffeb0a62be
Neimess <113237814+Neimess@users.noreply.github.com>
2023-03-17 19:13:03 +0300

    New methods

--- Maxim_Gavrilenko/Queue/queue.cpp
+++ Maxim_Gavrilenko/Queue/queue.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 template <typename T>
-class Queue {
+class queue {
 private:
     T *arr;
     int top;
@@ -10,9 +10,9 @@ private:
 
 public:
 
-    Queue(int);
+    queue(int);
 
-    ~Queue();
+    ~queue();
 
     bool isFull();
 
@@ -26,17 +26,17 @@ public:
 
     T get_tail();
 
+    void swap(queue<T>&);
+    
     int current_size();
 
-    void dequeue();
-
-    void clean();
+    void clear();
 
     void print();
 };
 
 template <typename T>
-Queue<T>::Queue(int num) {
+queue<T>::queue(int num) {
     arr = new T[num];
     top = -1;
     tail = -1;
@@ -45,90 +45,108 @@ Queue<T>::Queue(int num) {
 }
 
 template <typename T>
-Queue<T>::~Queue() {
+queue<T>::~queue() {
     delete[] arr;
 }
 
 template <typename T>
-bool Queue<T>::isFull() {
+bool queue<T>::isFull() {
     return tail == maxSize - 1;
 }
 
 template <typename T>
-bool Queue<T>::isEmpty() {
+bool queue<T>::isEmpty() {
     return top == -1 || top > tail;
 }
 
 template <typename T>
-void Queue<T>::push(T num) {
-    if (isFull()) throw std::domain_error("Queue is Full");
+void queue<T>::push(T num) {
+    if (isFull()) return;
     arr[++tail] = num;
     size++;
     if (top == -1) top = 0;
 }
 
 template <typename T>
-T Queue<T>::pop() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+T queue<T>::pop() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     size--;
     return arr[top++];
 }
 
 template <typename T>
-T Queue<T>::get_top() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+T queue<T>::get_top() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     return arr[top];
 }
 
 template <typename T>
-T Queue<T>::get_tail() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+T queue<T>::get_tail() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     return arr[tail];
 }
 
 template <typename T>
-int Queue<T>::current_size() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+int queue<T>::current_size() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     return size;
 }
 
-template <typename T>
-void Queue<T>::dequeue() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
-    while (!isEmpty()) {
-        std:: cout << pop() << " ";
-    }
-}
 
 template <typename T>
-void Queue<T>::clean() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+void queue<T>::clear() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     while (!isEmpty()) {
         pop();
     }
 }
 
 template <typename T>
-void Queue<T>::print() {
-    if (isEmpty()) throw std::domain_error("Queue is Empty");
+void queue<T>::print() {
+    if (isEmpty()) throw std::domain_error("queue is Empty");
     for(unsigned int i = 0; i < size; i++)
         std::cout << arr[i] << " ";
 }
 
+template <typename T>
+void queue<T>::swap(queue<T> & A) {
+    if (A.size != size) throw std::domain_error("queue size is not equal");
+    queue<T> temp(size);
+    memcpy(temp.arr,A.arr,sizeof(T) * size);
+    memcpy(A.arr,arr,sizeof(T) * A.size);
+    memcpy(arr,temp.arr, sizeof(T) * size);
+}
 int main() {
-    Queue<int> q(5);
+    queue<int> q(5);
+    queue<int> z(5);
     try {
         q.push(10);
         q.push(20);
         q.push(30);
         q.push(40);
         q.push(50);
+        z.push(30);
+        z.push(20);
+        z.push(1000);
+        z.push(21);
+        z.push(52);
         std::cout << "Front element: " << q.get_top() << std::endl;
-        std::cout << "Rear element: " << q.get_tail() << std:: endl;
-        std::cout << "Current_size:" << q.current_size() << std:: endl;
-        std::cout << "Print: "; q.print(); std:: cout << std:: endl;
+        std::cout << "Rear element: " << q.get_tail() << std::endl;
+        std::cout << "Current_size:" << q.current_size() << std::endl;
+        std::cout << "Print q: ";
+        q.print();
+        std::cout << "Print z: ";
+        z.print();
+        q.swap(z);
+        std::cout<<std::endl;
+        std::cout << "Print q:";
+        q.print();
+        std::cout<<std::endl;
+        std::cout << "Print z:";
+        z.print();
         std::cout << std::endl;
-        std::cout << "dequeue: "; q.dequeue(); std::cout << std:: endl;
+        std::cout << std::endl;
+        q.clear();
     }
     catch (std::domain_error& e) {
         std::cerr << "Caught: " << e.what() << std::endl;
