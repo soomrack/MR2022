538639e66b4ecb38861367e2378f424eb520db09
AntonPolyTech <COOLgay123@mail.ru>
2023-03-20 13:46:00 +0300

    Stack update: pop

--- Tyurin_Anton/PreorityQueue/PreorityQueue.cpp
+++ Tyurin_Anton/PreorityQueue/PreorityQueue.cpp
@@ -6,12 +6,14 @@ class PQueue;
 
 class Node {
     friend class PQueue;
+
 protected:
     Node *next;
+    Node *previous;
     std::string data;
     unsigned int priority;
 public:
-    Node(std::string data) : data(data), next(nullptr),priority(1) {}
+    Node(std::string data) : data(data), next(nullptr), previous(nullptr), priority(0) {}
 };
 
 class PQueue {
@@ -21,10 +23,15 @@ protected:
     unsigned int size;
 public:
     PQueue() : head(nullptr), tail(nullptr), size(0) {}
+
     bool is_empty();
+
     void push(std::string data, unsigned int priority);
+
     void print();
+
     void pop();
+
     unsigned int get_size();
 };
 
@@ -32,9 +39,9 @@ bool PQueue::is_empty() {
     return head == nullptr;
 }
 
-void PQueue::push(std::string data, unsigned int p){ // добавить пeремещение по приоритету вперед
+void PQueue::push(std::string data, unsigned int p) { // добавить пeремещение по приоритету вперед
     Node *local = new Node(data);
-    if(p > MAX_PRIORITY){
+    if (p > MAX_PRIORITY) {
         std::cout << "Maximal preority is " << MAX_PRIORITY << std::endl;
         p = MAX_PRIORITY;
     }
@@ -45,21 +52,20 @@ void PQueue::push(std::string data, unsigned int p){ // добавить пeре
         size++;
         return;
     }
-    Node* local1 = tail;
-    while(p > local1->next)
-    for (unsigned int idx = 0; idx < size; idx++){
-        for (unsigned int pty = 1; pty < MAX_PRIORITY; pty++){
-
+    Node *local1 = tail;
+    for (unsigned int idx = 0; idx < size; idx++) {
+        if(p <= local1->priority) {
+            local1->next = local1;
         }
     }
-    tail->next = local;
-    tail = local;
-    tail->priority = p;
+    local1->next = local;
+    local->priority = p;
+    //delete local1;
     size++;
 }
 
 void PQueue::print() {
-    if (is_empty()) {return;}
+    if (is_empty()) { return; }
     Node *local = head;
     while (local) {
         std::cout << local->data << " ";
@@ -70,29 +76,27 @@ void PQueue::print() {
 
 void PQueue::pop() {
     if (is_empty()) return;
-    Node* local = head;
+    Node *local = head;
     head = local->next;
     delete local;
     size--;
 }
 
-int main(){
+int main() {
     PQueue PQueue;
+    PQueue.push("he", 2);
+    PQueue.push("ll", 3);
+    //PQueue.push("ow", 1);
+    //PQueue.push(" ",1);
+    //PQueue.push("wo",1);
+    //PQueue.push("rl",2);
+    //PQueue.push("d",1);
     PQueue.print();
-    PQueue.push("he",1);
-    PQueue.push("ll",2);
-    PQueue.push("ow",2);
-    PQueue.push(" ",2);
-    PQueue.push("wo",2);
-    PQueue.push("rl",2);
-    PQueue.push("d",1);
-    PQueue.print();
-
-    PQueue.pop();
-    PQueue.print();
-    PQueue.pop();
-    PQueue.print();
-    PQueue.pop();
-    PQueue.print();
+    //PQueue.pop();
+    //PQueue.print();
+    //PQueue.pop();
+    //PQueue.print();
+    //PQueue.pop();
+   //PQueue.print();
     return 0;
 }
