50a39fdcf7a82d4722ebece3a78e590524654eed
Neimess <113237814+Neimess@users.noreply.github.com>
2023-03-20 20:11:39 +0300

    Redefinition of array

--- Maxim_Gavrilenko/Dinamic_array/Dinamic_array.cpp
+++ Maxim_Gavrilenko/Dinamic_array/Dinamic_array.cpp
@@ -1,28 +1,40 @@
 #include <iostream>
 #include "dynamic_array.h"
+#include <ctime>
 template <typename T>
 void print (dynamic_array<T>& A) {
-    for (unsigned int number = 0; number < A.lenght(); number++){
+    for (unsigned int number = 0; number < A.get_size(); number++){
         std::cout << A[number] << "  ";
     }
     std::cout << "\n";
 }
 int main() {
-    dynamic_array<double> A(10);
-    print(A);
-    A.insert(98.06, 0);
-    A.insert(7.04, 1);
-    A.insert(4.08, 2);
-    A.insert(3,1);
-    print(A);
-    std::cout << A.lenght() << std::endl;
-    A.replace(1.90, 3);
-    print(A);
-    A.search(4.08) ? std::cout << "YES" : std::cout << "NO";
-    std:: cout << std::endl;
-    A.resize(4);
-    print(A);
-    A.resize(10);
-    print(A);
+    try {
+        dynamic_array<double> A(10);;
+        for (unsigned i = 0; i < A.get_capacity(); i++)
+            A.insert(rand() % 10, i);
+        print(A);
+        std::cout << A.get_size() << std::endl;
+        A.replace(4, 3);
+        print(A);
+        A.search(4.08) ? std::cout << "YES" : std::cout << "NO";
+        std::cout << std::endl;
+        A.resize(12);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        print(A);
+        A.insert(3, 9);
+        A.insert(8, 2);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        A.insert(9, 2);
+        print(A);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        A.resize(10);
+        print(A);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+    }
+    catch (ARRAY_EXCEPTION &e)
+    {
+        std::cerr<<e.what()<<std::endl;
+    }
     return 0;
 }
--- Maxim_Gavrilenko/Dinamic_array/dynamic_array.h
+++ Maxim_Gavrilenko/Dinamic_array/dynamic_array.h
@@ -1,84 +1,87 @@
-
 #ifndef MR2022_DYNAMIC_ARRAY_H
 #define MR2022_DYNAMIC_ARRAY_H
+
 #include <algorithm>
+
+class ARRAY_EXCEPTION: public std::domain_error
+{
+public:
+    explicit ARRAY_EXCEPTION(const char* const str): std::domain_error(str) {};
+};
+
+ARRAY_EXCEPTION BADALLOC("Memory is not allocated");
+ARRAY_EXCEPTION OUTOFRANGE("Out of range");
 template <typename T>
 class dynamic_array {
 protected:
-    unsigned int size;
     unsigned int capacity;
+    unsigned int size;
     T* arr;
 
 
 public:
     dynamic_array();
-    dynamic_array(const unsigned int);
+    explicit dynamic_array(unsigned int);
     dynamic_array(const dynamic_array& A);
     dynamic_array(dynamic_array&& A) noexcept;
     ~dynamic_array();
 
-    T& operator[](const unsigned);
-    void resize_capacity(); // Увеличение размера в два раза
+    T& operator[](unsigned);
     void resize(unsigned int);
     void replace (T, T);
-    void insert (T, int);
+    void insert (T, unsigned int);
     bool search (T);
-    void fill_zero();
-    unsigned int lenght();
+    unsigned int get_size() {return size;}
+    unsigned int get_capacity() {return capacity;}
+    unsigned int get_free_capacity() {return capacity - size;}
     dynamic_array& operator=(const dynamic_array& A);
 };
 
 template <typename T>
-void dynamic_array<T>::fill_zero()
-{
-    for(unsigned int i = 0; i < capacity; i++)
-        arr[i] = 0;
-    size = capacity;
-}
-template <typename T>
 dynamic_array<T>::dynamic_array() {
     arr = nullptr;
-    size = 0;
     capacity = 0;
+    size = 0;
 }
 
 template <typename T>
-dynamic_array<T>::dynamic_array(const unsigned int number) {
+dynamic_array<T>::dynamic_array(const unsigned int value) {
+    capacity = value;
     size = 0;
-    capacity = number;
     arr = new T [capacity];
+    if (!arr) throw BADALLOC;
 }
 
 template <typename T>
 dynamic_array<T>::dynamic_array(const dynamic_array &array) : capacity(array.capacity), size(array.size) {
-    this->arr = new T[this->capacity];
-    if (!arr) throw std::bad_alloc();
-    memcpy(arr,array.arr,sizeof(T)*array.capacity);
+    this->arr = new T[this->size];
+    if (!arr) throw BADALLOC;
+    memcpy(arr,array.arr,sizeof(T)*array.size);
 }
 
 template <typename T>
-dynamic_array<T>::dynamic_array(dynamic_array&& array) noexcept : size(array.size), arr(array.arr),capacity(array.capacity) {
-    array.capacity = 0;
+dynamic_array<T>::dynamic_array(dynamic_array&& array) noexcept : capacity(array.capacity),size(array.size), arr(array.arr) {
     array.size = 0;
+    array.capacity = 0;
     array.arr = nullptr;
 }
 
 template <typename T>
 dynamic_array<T>::~dynamic_array() {
-    capacity = 0;
     size = 0;
+    capacity = 0;
     delete arr;
-    arr = nullptr;
 }
 
 template <typename T>
 dynamic_array<T>& dynamic_array<T>::operator=(const dynamic_array& A) {
     if (this!=&A)
         if(!arr) delete arr;
-    size = A.size;
     capacity = A.capacity;
-    arr = new T [size];
-    memcpy(arr,A.arr, size * sizeof(T));
+    size = A.size;
+    arr = new T [capacity];
+    if (!arr) throw BADALLOC;
+    memcpy(arr,A.arr, capacity * sizeof(T));
     return *this;
 }
 
@@ -90,33 +93,28 @@ bool dynamic_array<T>::search(T num) {
     return false;
 }
 
-template <typename T>
-void dynamic_array<T>::resize(unsigned int new_size) {
-    auto* new_data = new T[new_size];
-    memcpy(new_data,arr,sizeof(T) * new_size);
-    size = new_size;
-    delete arr;
-    arr = new_data;
-}
+
 
 template <typename T>
 void dynamic_array<T>::replace(T num, T den) {
-    for (unsigned int i = 0; i < size; i++) {
-        if (arr[i] == num) {
-            arr[i] = den;
+    if(search(num)){
+        for (unsigned int i = 0; i < size; i++) {
+            if (arr[i] == num) {
+                arr[i] = den;
+                break;
+            }
         }
     }
 }
 template <typename T>
-void dynamic_array<T>::insert(T value, int idx) {
-    if (idx > size || idx < 0) throw std::out_of_range("Out of range");
-    if (size == capacity) resize_capacity();
+void dynamic_array<T>::insert(T value, unsigned int idx)
+{
+    if (idx > capacity) throw OUTOFRANGE ;
+    if (size == capacity) resize(capacity * 2); // В случае конца массива, емкость расширяется в два раза
     std::copy(arr + idx, arr + size, arr + idx + 1);
     arr[idx] = value;
     size++;
 }
-template <typename T>
-unsigned int dynamic_array<T>::lenght() {return size;}
 
 template<typename T>
 T& dynamic_array<T>::operator[](const unsigned int idx)
@@ -124,17 +122,14 @@ T& dynamic_array<T>::operator[](const unsigned int idx)
     return arr[idx];
 }
 
-template<typename T>
-void dynamic_array<T>::resize_capacity()
-{
-    unsigned int new_capacity = capacity * 2;
-    T *new_arr = new T[new_capacity];
-
-    // копируем элементы в новый массив
-    memcpy(new_arr,arr,sizeof(T) * new_capacity);
-
-    delete[] arr;
-    arr = new_arr;
-    capacity = new_capacity;
+template <typename T>
+void dynamic_array<T>::resize(unsigned int new_size) {
+    if (new_size < size) size = new_size;
+    capacity = new_size;
+    auto* new_data = new T[capacity];
+    memcpy(new_data,arr,sizeof(T) * capacity);
+    delete arr;
+    arr = new_data;
 }
+
 #endif //MR2022_DYNAMIC_ARRAY_H
