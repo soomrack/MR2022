0d9089bbbdeb8df8f87a4e3eadd2553208864e99
dmitriychanchikov <terkin1512@gmail.com>
2023-03-20 18:10:01 +0300

    Dynamic Array (2-st version)

--- Chanchikov_Dmitriy/2 sem/Cpp/Dynamic Array.cpp
+++ Chanchikov_Dmitriy/2 sem/Cpp/Dynamic Array.cpp
@@ -9,56 +9,76 @@ class Dynamic_array{
 protected:
 
     unsigned int size;
+    unsigned int capacity;
+    float reserve;
 
 public:
     double* data;
 
     Dynamic_array();
-    explicit Dynamic_array(unsigned int size);
+    explicit Dynamic_array(unsigned int size_, float reserve_);
     Dynamic_array(const Dynamic_array& x);
-    ~Dynamic_array();
-
     Dynamic_array& operator=(const Dynamic_array& x);
+    ~Dynamic_array();
 
     void push_back(double elm);
     void pop_back();
-    unsigned int contain(double elm) const;  // возвращает позицию первого найденного элемента + 1;
-    void del(double elm) const;
-    void remove(double elm);
-    void clear();
+    void set(unsigned int pos, double elm);
+    void resize(unsigned int size_);
 
-    void shrink(unsigned int size_);
-    void extend(unsigned int size_);
-    void change_size(unsigned int size_);
-
-    unsigned int length() const;
     void output() const;
 
+    //unsigned int contain(double elm) const;
+    //void del(double elm) const;
+    //void remove(double elm);
+    //void clear();
+    //void shrink(unsigned int size_);
+    //void extend(unsigned int size_);
+    //unsigned int length() const;
 };
 
 
 Dynamic_array::Dynamic_array() {
     size = 0;
+    reserve = 0;
+    capacity = 0;
     data = nullptr;
-
 }
 
 
-Dynamic_array::Dynamic_array(const unsigned int size_) {
+Dynamic_array::Dynamic_array(const unsigned int size_, float reserve_) {
     size = size_;
-    data = new double[size];
-
+    reserve = reserve_;
+    capacity = unsigned(float(size) * (1 + reserve));  // capacity = size * (1 + reserve)
+    if (capacity - size < 1) capacity = size + 1;
+    data = new double[capacity];
 }
 
 
 Dynamic_array::Dynamic_array(const Dynamic_array& x) {
     size = x.size;
-
-    data = new double[size];
+    reserve = x.reserve;
+    capacity = x.capacity;
+    data = new double[capacity];
 
     for (unsigned int idx = 0; idx < x.size; idx++){
         data[idx] = x.data[idx];
     }
+}
+
+
+Dynamic_array &Dynamic_array::operator=(const Dynamic_array &x) {
+    if (this != &x){
+        if (!data) delete[] data;
+        size = x.size;
+        reserve = x.reserve;
+        capacity = x.capacity;
+        data = new double[capacity];
+        for (unsigned int idx = 0; idx < capacity; idx++){
+            data[idx] = x.data[idx];
+        }
+    }
+    return *this;
 }
 
 
@@ -67,42 +87,66 @@ Dynamic_array::~Dynamic_array() {
 }
 
 
-Dynamic_array &Dynamic_array::operator=(const Dynamic_array &x) {
-    if (this != &x){
-        if (!data) delete[] data;
-        size = x.size;
-        data = new double[size];
-        for (unsigned int idx = 0; idx < size; idx++){
-            data[idx] = x.data[idx];
-        }
-    }
-
-    return *this;
-}
-
-
 void Dynamic_array::push_back(double elm) {
-    //double temp[size + 1];
-    auto* temp = new double[size + 1];
-    for (unsigned int idx = 0; idx < size; idx++){
-        temp[idx] = data[idx];
+    if (capacity < size + 1) {
+        capacity = unsigned(float(size) * (1 + reserve));  // capacity = size * (1 + reserve)
+        auto* temp = new double[capacity];
+        for (unsigned int idx = 0; idx < size; idx++){
+            temp[idx] = data[idx];
+        }
+        delete[] data;
+        data = temp;
     }
-    delete[] data;
-    data = temp;
     data[size++] = elm;
 }
 
 
 void Dynamic_array::pop_back() {
-    auto* temp = new double[--size];
-    for (unsigned int idx = 0; idx < size; idx++){
-        temp[idx] = data[idx];
-    }
-    //delete[] data;
-    data = temp;
-
+    data[size - 1] = 0;
+    size--;
 }
 
+
+void Dynamic_array::set(unsigned int pos, double elm) {
+    push_back(0);
+    for (unsigned int idx = size; idx > pos; idx--)
+        data[idx] = data[idx - 1];
+    data[pos] = elm;
+}
+
+
+void Dynamic_array::resize(unsigned int size_) {
+    if (capacity < size_) {
+        capacity = unsigned(float(size_) * (1 + reserve));  // capacity = size_ * (1 + reserve)
+        auto* temp = new double[capacity];
+        for (unsigned int idx = 0; idx < size_; idx++){
+            temp[idx] = data[idx];
+        }
+        delete[] data;
+        data = temp;
+    }
+    size = size_;
+}
+
+
+void Dynamic_array::output() const {
+    std::cout << "Length: " << size << "\n";
+    if (size > 0) {
+        for (unsigned int idx = 0; idx < size; idx++) {
+            if (fabs(data[idx]) > COMPARATION_CONST && fabs(data[idx]) < pow(COMPARATION_CONST, -1))
+                std::cout << data[idx] << " ";
+            else
+                std::cout << "0 ";
+        }
+        std::cout << "\n";
+    }
+    std::cout << "\n";
+}
+
+/*
+unsigned int Dynamic_array::length() const {
+    return size;
+}
 
 unsigned int Dynamic_array::contain(double elm) const {
     for (unsigned int idx = 0; idx < size; idx++){
@@ -166,67 +210,31 @@ void Dynamic_array::extend(unsigned int size_) {
     data = temp;
     size = size_;
 }
-
-void Dynamic_array::change_size(unsigned int size_) {  // shrink + extend
-    auto* temp = new double[size_];
-    for (unsigned int idx = 0; idx < size_; idx++){
-        temp[idx] = data[idx];
-    }
-    delete[] data;
-    data = temp;
-    size = size_;
-}
-
-
-unsigned int Dynamic_array::length() const {
-    return size;
-}
-
-
-void Dynamic_array::output() const {
-    std::cout << "Length: " << size << "\n";
-
-    if (size > 0) {
-
-        for (unsigned int idx = 0; idx < size; idx++) {
-            if (fabs(data[idx]) > COMPARATION_CONST && fabs(data[idx]) < pow(10, 64))
-                std::cout << data[idx] << " ";
-            else
-                std::cout << "0 ";
-        }
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-}
-
+*/
 
 int main() {
-    Dynamic_array A;
-    Dynamic_array B(10);
-    Dynamic_array C(B);
+    //Dynamic_array A;
+    Dynamic_array B(10, 1);
+    B.output();
+    //Dynamic_array C(B);
 
     B.data[0] = 11.7;
-    B.push_back(-13);
     B.data[5] = 15;
-    C.data[5] = 15;
-
-    A.output();
-    B.output();
-    C.output();
-
-    B.remove(11.7);
-    B.remove(-13);
-    B.remove(15);
+    B.push_back(-13);
     B.output();
 
     B.pop_back();
-    B.extend(10);
-    B.shrink(2);
-    //B.change_size(100);
     B.output();
-    //B.clear();
+
+    B.set(5, 123);
+    B.output();
+
+    B.resize(20);
+    B.output();
 
 
-    //B.output();
+    B.resize(1);
+    B.output();
+
     return 0;
 }
