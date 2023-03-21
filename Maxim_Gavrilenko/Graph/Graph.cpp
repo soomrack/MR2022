9c6e1435a2c0d7bb261ffcc266d291e662476bf7
Neimess <113237814+Neimess@users.noreply.github.com>
2023-03-21 16:42:14 +0300

    Graph on templates

--- Maxim_Gavrilenko/Graph/Graph.cpp
+++ Maxim_Gavrilenko/Graph/Graph.cpp
@@ -1,11 +1,12 @@
 #include "Graph.h"
 int main() {
-    Graph graph(5);
+    Graph<double> graph(5);
     graph.addEdge(0, 1);
     graph.addEdge(0, 4);
     graph.addEdge(1, 3);
     graph.addEdge(2,3);
     graph.addEdge(1,2);
+    graph.addVertex(7);
     graph.removeEdge(0,1);
     graph.removeVertex(1);
 
--- Maxim_Gavrilenko/Graph/Graph.h
+++ Maxim_Gavrilenko/Graph/Graph.h
@@ -4,106 +4,50 @@
 #include <iostream>
 #include <C:\Users\El1x3r7714\ClionProjects\MR2022\Maxim_Gavrilenko\Linked_list\dual_linked_list.h>
 
-
-
-/*class Graph {
-private:
-    int V; // Количество вершин
-    std::list<int> *adj; // Указатель на массив, содержащий списки смежности
-public:
-    Graph(int V) {
-        this->V = V;
-        adj = new std::list<int>[V];
-    }
-
-    // Добавление ребра в граф
-    void addEdge(int v, int w) {
-        adj[v].push_back(w); // Добавление вершины w в список v
-        adj[w].push_back(v); // Добавление вершины v в список w
-    }
-
-    // Удаление ребра из графа
-    void removeEdge(int v, int w) {
-        adj[v].remove(w); // Удаление вершины w из списка v
-        adj[w].remove(v); // Удаление вершины v из списка w
-    }
-
-    // Добавление вершины в граф
-    void addVertex(int v) {
-        if (v < V) {
-            return; // Вершина уже существует
-        }
-        V++;
-        std::list<int> *newAdj = new std::list<int>[V];
-        for (int i = 0; i < V - 1; i++) {
-            newAdj[i] = adj[i];
-        }
-        newAdj[V - 1] = std::list<int>();
-        adj = newAdj;
-    }
-
-    // Удаление вершины из графа
-    void removeVertex(int v) {
-        if (v >= V) {
-            return; // Вершина не существует
-        }
-        V--;
-        std::list<int> *newAdj = new std::list<int>[V];
-        for (int i = 0, j = 0; i < V + 1; i++) {
-            if (i != v) {
-                newAdj[j++] = adj[i];
-            }
-        }
-        adj = newAdj;
-    }
-
-};*/
-
-
-
+template <typename T>
 class Graph {
 private:
-    int V; // Количество вершин
-    LinkedList *adj; // Указатель на массив, содержащий списки смежности
+    unsigned int V; // Количество вершин
+    LinkedList<T> *adj; // Указатель на массив, содержащий списки смежности
 public:
-    Graph(int V) {
+    Graph(unsigned int V) {
         this->V = V;
-        adj = new LinkedList[V];
+        adj = new LinkedList<T>[this->V];
     }
 
     // Добавление ребра в граф
-    void addEdge(int v, int w) {
+    void addEdge(unsigned int v, int w) {
         adj[v].push_tail(w); // Добавление вершины w в список v
         adj[w].push_tail(v); // Добавление вершины v в список w
     }
 
     // Удаление ребра из графа
-    void removeEdge(int v, int w) {
-        adj[v].remove(w); // Удаление вершины w из списка v
-        adj[w].remove(v); // Удаление вершины v из списка w
+    void removeEdge(unsigned int s, unsigned int w) {
+        adj[s].remove(w); // Удаление вершины w из списка s
+        adj[w].remove(s); // Удаление вершины s из списка w
     }
 
     // Добавление вершины в граф
-    void addVertex(int v) {
+    void addVertex(unsigned int v) {
         if (v < V) {
             return; // Вершина уже существует
         }
         V++;
-        LinkedList *newAdj = new LinkedList[V];
+        LinkedList<T> *newAdj = new LinkedList<T>[V];
         for (int i = 0; i < V - 1; i++) {
             newAdj[i] = adj[i];
         }
-        newAdj[V - 1] = LinkedList();
+        newAdj[V - 1] = LinkedList<T>();
         adj = newAdj;
     }
 
     // Удаление вершины из графа
-    void removeVertex(int v) {
+    void removeVertex(unsigned int v) {
         if (v >= V) {
             return; // Вершина не существует
         }
         V--;
-        LinkedList *newAdj = new LinkedList[V];
+        LinkedList<T> *newAdj = new LinkedList<T>[V];
         for (int i = 0, j = 0; i < V + 1; i++) {
             if (i != v) {
                 newAdj[j++] = adj[i];
