c75d89ea114c5d883b6a4251fdaf94ed13a7bd21
Neimess <113237814+Neimess@users.noreply.github.com>
2023-03-21 11:16:00 +0300

    Remake graph on selfmade dual_linked_list

--- Maxim_Gavrilenko/Graph/Graph.cpp
+++ Maxim_Gavrilenko/Graph/Graph.cpp
@@ -1,5 +1,4 @@
 #include "Graph.h"
-#include "iostream"
 int main() {
     Graph graph(5);
     graph.addEdge(0, 1);
@@ -7,8 +6,8 @@ int main() {
     graph.addEdge(1, 3);
     graph.addEdge(2,3);
     graph.addEdge(1,2);
-    std::cout << "DFS traversal starting from vertex 0:" << std::endl;
-
+    graph.removeEdge(0,1);
+    graph.removeVertex(1);
 
     return 0;
 
--- Maxim_Gavrilenko/Graph/Graph.h
+++ Maxim_Gavrilenko/Graph/Graph.h
@@ -2,11 +2,11 @@
 #ifndef MR2022_GRAPH_H
 #define MR2022_GRAPH_H
 #include <iostream>
-#include <list>
+#include <C:\Users\El1x3r7714\ClionProjects\MR2022\Maxim_Gavrilenko\Linked_list\dual_linked_list.h>
 
 
 
-class Graph {
+/*class Graph {
 private:
     int V; // Количество вершин
     std::list<int> *adj; // Указатель на массив, содержащий списки смежности
@@ -56,5 +56,61 @@ public:
         }
         adj = newAdj;
     }
+
+};*/
+
+
+
+class Graph {
+private:
+    int V; // Количество вершин
+    LinkedList *adj; // Указатель на массив, содержащий списки смежности
+public:
+    Graph(int V) {
+        this->V = V;
+        adj = new LinkedList[V];
+    }
+
+    // Добавление ребра в граф
+    void addEdge(int v, int w) {
+        adj[v].push_tail(w); // Добавление вершины w в список v
+        adj[w].push_tail(v); // Добавление вершины v в список w
+    }
+
+    // Удаление ребра из графа
+    void removeEdge(int v, int w) {
+        adj[v].remove(w); // Удаление вершины w из списка v
+        adj[w].remove(v); // Удаление вершины v из списка w
+    }
+
+    // Добавление вершины в граф
+    void addVertex(int v) {
+        if (v < V) {
+            return; // Вершина уже существует
+        }
+        V++;
+        LinkedList *newAdj = new LinkedList[V];
+        for (int i = 0; i < V - 1; i++) {
+            newAdj[i] = adj[i];
+        }
+        newAdj[V - 1] = LinkedList();
+        adj = newAdj;
+    }
+
+    // Удаление вершины из графа
+    void removeVertex(int v) {
+        if (v >= V) {
+            return; // Вершина не существует
+        }
+        V--;
+        LinkedList *newAdj = new LinkedList[V];
+        for (int i = 0, j = 0; i < V + 1; i++) {
+            if (i != v) {
+                newAdj[j++] = adj[i];
+            }
+        }
+        adj = newAdj;
+    }
 };
+
 #endif //MR2022_GRAPH_H
