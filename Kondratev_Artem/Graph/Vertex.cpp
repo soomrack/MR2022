efb7eeed510344f81ea7b09b8a52685d3f82d447
artem-kondratew <artemkondratev5@gmail.com>
2023-03-26 13:13:29 +0300

    created

---
+++ Kondratev_Artem/Graph/Vertex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 3/26/23.
+//
+
+#include "Vertex.h"
---
+++ Kondratev_Artem/Graph/Vertex.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 3/26/23.
+//
+
+#ifndef HELLO_WORLD_VERTEX_H
+#define HELLO_WORLD_VERTEX_H
+
+
+class Vertex {
+
+};
+
+
+#endif //HELLO_WORLD_VERTEX_H
