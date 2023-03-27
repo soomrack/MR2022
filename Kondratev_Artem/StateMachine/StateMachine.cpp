08c2a92c482f58ac6e75177554b3d6f7960a3d70
artem-kondratew <artemkondratev5@gmail.com>
2023-03-27 13:24:23 +0300

    StateMachine created

---
+++ Kondratev_Artem/StateMachine/StateMachine.cpp
@@ -0,0 +1,5 @@
+//
+// Created by user on 3/27/23.
+//
+
+#include "StateMachine.h"
---
+++ Kondratev_Artem/StateMachine/StateMachine.h
@@ -0,0 +1,14 @@
+//
+// Created by user on 3/27/23.
+//
+
+#ifndef HELLO_WORLD_STATEMACHINE_H
+#define HELLO_WORLD_STATEMACHINE_H
+
+
+class StateMachine {
+
+};
+
+
+#endif //HELLO_WORLD_STATEMACHINE_H
