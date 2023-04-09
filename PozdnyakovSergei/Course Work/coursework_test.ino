23ac40d0c8647f9385c584767ece84b79be5ab54
sergei-pozd25 <113307957+sergei-pozd25@users.noreply.github.com>
2023-04-09 21:34:28 +0300

    changed functions

--- PozdnyakovSergei/Course Work/coursework_test.ino
+++ PozdnyakovSergei/Course Work/coursework_test.ino
@@ -14,11 +14,13 @@
 #define dur_pin_l = 7  // направление левых моторов
 
 bool is_on = false;  // для кнопки включения
+bool button_state = false;
 float time = 0.0;
 int soudn_time = 0;  // время оповещения
 float filt_param = 0.2;  // параметр для фильтрации
 float dist_filt = 0;  // отфильтрованный выход
 float dur_param = 29.1;
+
 
 void setup() {
   Serial.begin(9600);
@@ -34,11 +36,11 @@ void setup() {
 
 
 void start_motors () {
-  
+
 }
 
 
-float get_distance() {
+int get_distance() {
   long dur, sm;
   trigPin = digitalWrite(LOW);
   delayMicroseconds(2);
@@ -54,6 +56,13 @@ float get_distance() {
   sm = map(sm, 0, 350, 0, 255);
   //sound_time = map(sm, 0, 255, 700, 100);
   return (sm);
+}
+
+
+int dist_filtered() {
+  int dist = get_distance();
+  dist_filt += (dist - dist_filt) * filt_param;
+  return (dist_filt);
 }
 
 
@@ -73,20 +82,17 @@ void sound() {
 }
 
 
-
-void dist_filtered() {
-  float dist = get_distance();
-  dist_filt += (dist - dist_filt) * filt_param;
-  return (dist_filt);
+bool check_button() {
+  if (digitalRead (buttonPin) == HIGH) {
+    button_state = not button_state;
+    delay(100);
+  }
+  return button_state;
 }
 
 
 void loop() {
-  if (digitalRead (buttonPin) == HIGH) {
-    is_on = not is_on;
-    current mode = 0;
-    delay(1000);
-  }
+  bool is_on = check_button();
 
   if (is_on) {
     start_motors();
