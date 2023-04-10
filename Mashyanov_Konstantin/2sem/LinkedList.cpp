17739110310029ad86a8427f43dd76580632cd91
Mashyanovk <114390812+Mashyanovk@users.noreply.github.com>
2023-04-10 15:41:16 +0300

    Исправлены имена методов

--- Mashyanov_Konstantin/2sem/LinkedList.cpp
+++ Mashyanov_Konstantin/2sem/LinkedList.cpp
@@ -9,25 +9,23 @@ class List
 	{
 	public:
 		T data;  //  Реализация объекта (без привязки к типу данных) 
-		Node* next;
+		Node *next;
 
-		Node(T t = T(), Node* n = nullptr);  //  Конструктор Класса 
+		Node(T t = T(), Node *n = nullptr);  //  Конструктор Класса 
 		};
-	Node* head;  //  Голова списка
-	int size;  //  Количество элементов
-
+	Node *head;  //  Голова списка
+	
 public:
 	List();  //  Конструктор 
 	~List();  //  Деструктор
-	void pushBack(T value);  //  Метод добавления элемента в конец списка 
-	void pushFront(T value);  //  Метод добавления элемента в начало списка
-	int getSize() const;  //  Метод для объявления размера
-	T& operator[] (int);  //  Возврат элемента методом счётчика индекса
-	void popFront();  //  Метод удаления первого элемента
-	void popBack();   //  Метод удаления последнего элемента
+	void push_tail(T value);  //  Метод добавления элемента в конец списка 
+	void push_head(T value);  //  Метод добавления элемента в начало списка
+	void pop_tail();  //  Метод удаления первого элемента
+	void pop_head();   //  Метод удаления последнего элемента
+	void delete_head();  //  Удаление первого
+        void delete_tail();  //  Удаление последнего 
 	void clear();  //  Метод очистки данных 
-	void insert(T value, int index);  //  Метод добавления элемента по конкретному индексу
-	void removeAt(int index);  //  Метод удаления элемента из списка по указанному индексу
+        void delete_position(int pos);
 };
 
 template<class T>
@@ -37,10 +35,10 @@ int main()
 {
 	setlocale(0, "");
 	List<int> list;
-	for (int i = 0; i < 5; i++) list.pushBack(i);
+	for (int i = 0; i < 5; i++) list.push_tail(i);
 	show(list);
-	list.popBack();
-	list.popFront();
+	list.pop_tail();
+	list.pop_head();
 	list[1] = 777;
 	show(list);
 	system("pause");
@@ -67,7 +65,7 @@ List<T>::~List()
 }
 
 template<class T>
-void List<T>::pushBack(T value)
+void List<T>::push_tail(T value)
 {
 	if (head == nullptr)  //  Проверка вершины на null 
 		head = new Node(value);
@@ -84,45 +82,25 @@ void List<T>::pushBack(T value)
 }
 
 template<class T>
-void List<T>::pushFront(T value)
+void List<T>::push_head(T value)
 {
 	head = new Node(value, head);
 	size++;
 }
 
-template<class T>
-int List<T>::getSize() const
-{
-	return size;
-}
 
 template<class T>
-T& List<T>::operator[](int index)
-{
-	if (index > size - 1 || index < 0)
-	{
-		string message = "Недопустимый индекс ";
-		message.append(to_string(index));
-		throw out_of_range(message);
-	}
-	Node* current = head;
-	for (int i = 0; i < index; i++)
-		current = current->next;
-	return current->data;
-}
-
-template<class T>
-void List<T>::popFront()
+void List<T>::pop_head()
 {
 	if (size <= 0) return;
-	Node* temp = head;  //  Временная переменная temp
+	Node *temp = head;  //  Временная переменная temp
 	head = head->next;
 	delete temp;
 	size--;  //  Декремент размера списка
 }
 
 template<class T>
-void List<T>::popBack()
+void List<T>::pop_tail()
 {
 	removeAt(size - 1);
 }
@@ -130,50 +108,43 @@ void List<T>::popBack()
 template<class T>
 void List<T>::clear()
 {
-	while (size) popFront();
+	while (size) pop_head();
 }
 
-template<class T>
-void List<T>::insert(T value, int index)
-{
-	if (index > size || index < 0)
-	{
-		string message = "Недопустимый индекс ";
-		message.append(to_string(index));
-		throw out_of_range(message);
-	}
-	if (index == 0) pushFront(value);
-	else
-	{
-		Node* previous = head;  //  Цикл для нахождения предшествующего индекса
-		for (int i = 0; i < index - 1; i++)
-			previous = previous->next;
-		previous->next = new Node(value, previous->next);
-		size++;
-	}
-}
+  void List::delete_head(){
+        Node *temp = new Node;
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
 
-template<class T>
-void List<T>::removeAt(int index)  //  Метод обратный insert
-{
-	if (index > size - 1 || index < 0)
-	{
-		string message = "Недопустимый индекс ";
-		message.append(to_string(index));
-		throw out_of_range(message);
-	}
-	if (index == 0) popFront();
-	else
-	{
-		Node* previous = head;
-		for (int i = 0; i < index - 1; i++)
-			previous = previous->next;
-		Node* temp = previous->next;
-		previous->next = temp->next;
-		delete temp;
-		size--;
-	}
-}
+    void List::delete_tail()
+    {
+        Node *current = new Node;
+        Node *previous = new Node;
+        current = head;
+        while(current->next != nullptr)
+        {
+            previous = current;
+            current = current->next;
+        }
+        tail = previous;
+        previous->next = NULL;
+        delete current;
+    }
+
+    void List::delete_position(int pos){
+        Node *current = new Node;
+        Node *previous = new Node;
+        current = head;
+        for(int i=1;i<pos;i++)
+        {
+            previous = current;
+            current = current->next;
+        }
+        previous->next = current->next;
+    }
+
 
 template<class T>
 void show(List<T>& list)
