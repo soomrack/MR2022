b8732470337858e406d4f5fa2217da3509f2813c
AntonPolyTech <COOLgay123@mail.ru>
2023-04-17 13:44:07 +0300

    Graph delete update

--- Tyurin_Anton/Graph/main.cpp
+++ Tyurin_Anton/Graph/main.cpp
@@ -4,16 +4,18 @@ int main() {
     Graph graph;
 
     graph.addNode(0);
-    //graph.addNode(1);
+    graph.addNode(1);
     //graph.addNode(22);
 
-    //graph.addEdge(0, 1);
+    graph.addEdge(0, 1);
     //graph.addEdge(0, 22);
 
     graph.deleteNode(0);
     //graph.deleteNode(1);
     //graph.deleteNode(22);
     if (graph.node_list.is_empty()){std::cout << "no Nodes";}
+    std::cout << "Size is " << graph.findGraphNode(1)->edge_list.get_size();
+
 
     return 0;
 }
