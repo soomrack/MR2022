0fe6748ef6498c0a4c64cd23975a49288f519d96
artem-kondratew <artemkondratev5@gmail.com>
2023-04-17 14:23:39 +0300

    deleteEdges while deleteVertex

--- Tyurin_Anton/Graph/main.cpp
+++ Tyurin_Anton/Graph/main.cpp
@@ -4,13 +4,20 @@ int main() {
     Graph graph;
 
     graph.addNode(0);
+    graph.addNode(1);
+    graph.addNode(2);
+    graph.addNode(3);
+    graph.addNode(4);
+
+    //graph.addNode(1);
+    //std::cout << graph.findGraphNode(0)->operator==(graph.findGraphNode(0));
     //graph.addNode(1);
     //graph.addNode(22);
 
     //graph.addEdge(0, 1);
     //graph.addEdge(0, 22);
 
-    graph.deleteNode(0);
+    graph.deleteNode(1);
     //graph.deleteNode(1);
     //graph.deleteNode(22);
     if (graph.node_list.is_empty()){std::cout << "no Nodes";}
