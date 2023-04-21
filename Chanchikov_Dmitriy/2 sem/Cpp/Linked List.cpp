c406e1719c50f81ddb0550debf0f2b1406fbda0e
dmitriychanchikov <terkin1512@gmail.com>
2023-04-21 16:25:22 +0300

    Linked List (4-th version): полностью переписал программу, постарался все сделать максимально просто

--- Chanchikov_Dmitriy/2 sem/Cpp/Linked List.cpp
+++ Chanchikov_Dmitriy/2 sem/Cpp/Linked List.cpp
@@ -1,235 +1,145 @@
 #include <iostream>
-#include <exception>
-
-const double COMPARATION_CONST = 0.0001;
-
-
-double abs(double x){
-    if (x < COMPARATION_CONST) return -x;
-    return x;
-}
-
 
 class Item {
-
-protected:
-    Item* next_Item;
-    Item* prev_Item;
-
 public:
-    double value;
+    explicit Item(double val) : data(val), next(nullptr), prev(nullptr) {}
 
-    Item();
-    explicit Item(double x);
-    ~Item();
-
-    void push_next(Item* x);
-    void push_prev(Item* x);
-
-    Item* next();
-    Item* prev();
-
-    void del_current(Item *&list_head, Item *&list_tail);
-
+    double data;
+    Item* next;
+    Item* prev;
 };
 
-
 class LinkedList {
-
-protected:
-
+private:
+    Item* head;
+    Item* tail;
 
 public:
-    Item* list_head;
-    Item* list_tail;
-
-    LinkedList();
-    explicit LinkedList(double list_head_val);
-    ~LinkedList();
+    LinkedList() : head(nullptr), tail(nullptr) {}
 
     void push_head(double val);
     void push_tail(double val);
 
-    Item* pop(double val);
-
-    Item* head();
-    Item* tail();
+    double pop_head();
+    double pop_tail();
+    //void pop(double val);
 
     void print();
-
 };
 
-
-
-Item::Item() {
-    value = double(NULL);
-    next_Item = nullptr;
-    prev_Item = nullptr;
-}
-
-
-Item::Item(const double x) {
-    value = x;
-    next_Item = nullptr;
-    prev_Item = nullptr;
-}
-
-
-Item::~Item() {
-    next_Item = nullptr;
-    prev_Item = nullptr;
-}
-
-
-Item* Item::next() {
-    return next_Item;
-}
-
-
-Item* Item::prev() {
-    return prev_Item;
-}
-
-
-void Item::push_next(Item *x) {
-    if (x == nullptr) return;
-    next_Item = x;
-    x->prev_Item = this;
-}
-
-
-void Item::push_prev(Item *x) {
-    if (x == nullptr) return;
-    prev_Item = x;
-    x->next_Item = this;
-}
-
-
-void Item::del_current(Item *&list_head, Item *&list_tail) {
-    if (prev_Item != nullptr) {
-        prev_Item->next_Item = next_Item;
-    }
-    if (next_Item != nullptr) {
-        next_Item->prev_Item = prev_Item;
-    }
-    if (this == list_head) {
-        list_head = next_Item;
-    }
-    if (this == list_tail) {
-        list_tail = prev_Item;
-    }
-    prev_Item = nullptr;
-    next_Item = nullptr;
-    delete this;
-}
-
-
-LinkedList::LinkedList() {
-    list_head = nullptr;
-    list_tail = nullptr;
-}
-
-
-LinkedList::LinkedList(double list_head_value) {
-    list_head = nullptr;
-    list_tail = nullptr;
-    push_head(list_head_value);
-}
-
-
-LinkedList::~LinkedList() {
-    list_head = nullptr;
-    list_tail = nullptr;
-}
-
-
+// Добавление элемента в начало списка
 void LinkedList::push_head(double val) {
-    Item* elm = new Item(val);
-    if (elm == nullptr) return;
-    if (list_head == nullptr){
-        list_head = elm;
-        list_tail = elm;
-    }
-    else {
-        elm->push_next(list_head);
-        list_head->push_prev(elm);
-        list_head = elm;
-    }
-}
-
-
-void LinkedList::push_tail(double val) {
-    Item* elm = new Item(val);
-    if (list_head == nullptr){
-        list_head = elm;
-        list_tail = elm;
+    Item* new_item = new Item(val);
+    if (!head) {
+        head = tail = new_item;
     } else {
-        list_tail->push_next(elm);
-        elm->push_prev(list_tail);
-        list_tail = elm;
+        head->prev = new_item;
+        new_item->next = head;
+        head = new_item;
     }
 }
 
+// Добавление элемента в конец списка
+void LinkedList::push_tail(double val) {
+    Item* new_item = new Item(val);
+    if (!head) {
+        head = tail = new_item;
+    } else {
+        tail->next = new_item;
+        new_item->prev = tail;
+        tail = new_item;
+    }
+}
 
-Item* LinkedList::pop(double val) {
-    if (list_head == nullptr) return nullptr;
-    Item* current = list_head;
-    do {
-        if (abs(current->value - val) < COMPARATION_CONST){
-            if (current == list_head) list_head = current->next();
-            if (current == list_tail) list_tail = current->prev();
-            current->del_current(list_head, list_tail);
-            return current;
+// Удаление первого элемента списка и возвращение его значение
+double LinkedList::pop_head() {
+    if (!head) {
+        throw std::runtime_error("List is empty");
+    }
+    double value = head->data;
+    Item* tmp = head;
+    head = head->next;
+    if (head) {
+        head->prev = nullptr;
+    } else {
+        tail = nullptr;
+    }
+    delete tmp;
+    return value;
+}
+
+// Удаление последнего элемента списка и возвращение его значение
+double LinkedList::pop_tail() {
+    if (!tail) {
+        throw std::runtime_error("List is empty");
+    }
+    double value = tail->data;
+    Item* tmp = tail;
+    tail = tail->prev;
+    if (tail) {
+        tail->next = nullptr;
+    } else {
+        head = nullptr;
+    }
+    delete tmp;
+    return value;
+}
+
+// Удаление элемента по значению
+/*
+void LinkedList::pop(double val) {
+    Item* current = head;
+    while (current) {
+        if (current->data == val) {
+            if (current == head && current == tail) {
+                head = tail = nullptr;
+            } else if (current == head) {
+                head = head->next;
+                head->prev = nullptr;
+            } else if (current == tail) {
+                tail = tail->prev;
+                tail->next = nullptr;
+            } else {
+                current->prev->next = current->next;
+                current->next->prev = current->prev;
+            }
+            delete current;
+            return;
         }
-        current = current->next();
-    } while (current != nullptr);
-    return nullptr;
-}
-
-
-Item* LinkedList::head() {
-    return list_head;
-}
-
-
-Item* LinkedList::tail() {
-    return list_tail;
-}
-
-
-void LinkedList::print() {
-    if (head() == nullptr) return;
-    std::cout << "The list_head is " << list_head->value << ";  the list_tail is " << list_tail->value << "\n";
-    Item* current = list_head;
-    while (current != nullptr){
-        std::cout << current->value << "  ";
-        if (current->prev() != nullptr) std::cout << "his prev is " << current->prev()->value << "  ";
-        if (current->prev() != nullptr && current->next() != nullptr) std::cout << "and ";
-        if (current->next() != nullptr) std::cout << "his next is " << current->next()->value;
-        std::cout << "\n";
-        current = current->next();
+        current = current->next;
     }
-    std::cout << "\n";
 }
+*/
 
+// Вывод элементов списка
+void LinkedList::print() {
+    Item* current = head;
+    std::cout << "List: ";
+    while (current) {
+        std::cout << current->data << " ";
+        current = current->next;
+    }
+    std::cout << std::endl;
+}
 
 int main() {
+    LinkedList list;
 
-    LinkedList A = LinkedList();
-    A.push_head(10.1);
-    A.print();
+    // Добавление элементов в начало и конец списка
+    list.push_head(2.5);
+    list.push_head(-1.8);
+    list.push_tail(3.789);
 
-    LinkedList B = LinkedList(0.1);
-    B.print();
-    B.push_head(1.2);
-    B.print();
-    B.push_tail(2.3);
-    B.print();
+    list.print();
 
-    B.pop(2.3);
-    B.print();
+    // Извлечение первого и последнего элементов списка
+    double head_val = list.pop_head();
+    double tail_val = list.pop_tail();
 
-    LinkedList C = LinkedList(1);
+    std::cout << "First element: " << head_val << std::endl;
+    std::cout << "Last element: " << tail_val << std::endl;
+
+    list.print();
 
     return 0;
 }
