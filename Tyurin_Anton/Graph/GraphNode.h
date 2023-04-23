8ccfc9f179a989e1bc48c618ba4d882a9325a3e0
artem-kondratew <artemkondratev5@gmail.com>
2023-04-23 21:57:02 +0300

    backup

--- Tyurin_Anton/Graph/GraphNode.h
+++ Tyurin_Anton/Graph/GraphNode.h
@@ -21,9 +21,9 @@ public:
         return data;
     };
 
-    bool operator==(const GraphNode *right) const {
-        if (this->data == right->data) {return true;}
-    return false;
+    bool operator==(const GraphNode& right) const {
+        if (this->data == right.data) {return 1;}
+    return 0;
     }
 
 
