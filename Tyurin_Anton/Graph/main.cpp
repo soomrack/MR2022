ec3829b510400151695c976ac58b6d57035c97ff
AntonPolyTech <COOLgay123@mail.ru>
2023-04-27 11:53:27 +0300

    Graph deleteNode done

--- Tyurin_Anton/Graph/main.cpp
+++ Tyurin_Anton/Graph/main.cpp
@@ -11,7 +11,7 @@ int main() {
     graph.addEdge(0, 22);
 
     graph.deleteNode(0);
-    //graph.deleteNode(1);
+    graph.deleteNode(1);
     graph.deleteNode(22);
     if (graph.node_list.is_empty()){std::cout << "no Nodes";}
     //if (graph.findGraphNode(1)->edge_list.is_empty()) { std::cout << "no Location " << std::endl;}
