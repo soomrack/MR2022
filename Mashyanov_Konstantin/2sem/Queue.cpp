961785fb144edb775111891c2b4806a4aa125561
Mashyanovk <114390812+Mashyanovk@users.noreply.github.com>
2023-05-10 18:44:41 +0300

    Update Queue.cpp
    Добавлены методы push и pop, убраны исключения

--- Mashyanov_Konstantin/2sem/Queue.cpp
+++ Mashyanov_Konstantin/2sem/Queue.cpp
@@ -3,83 +3,59 @@ using namespace std;
 
 class Queue {
 private:
-  int* data;  
-  int front;  // Индекс элемента, находящегося в начале очереди
-  int rear;   // Индекс элемента, находящегося в конце очереди
-  int size;   
+int* data;
+int front; // Индекс элемента, находящегося в начале очереди
+int rear; // Индекс элемента, находящегося в конце очереди
+int size;
 
 public:
-  Queue(int n);
-  ~Queue();
-  int get_front();  //  Метод для получения первый элемент очереди
-  int get_rear();  //  Метод для получения последнего элемента очереди
-  void enqueue(int value);  //  Метод для добавления элемента в конец очереди
-  void dequeue();  //  Метод для удаления элемента из начала очереди
-  void display();  //  Метод для отображения элементов в очереди
-  bool is_empty();  // Метод для проверки, пуста ли очередь
-  bool is_full();  // Метод для проверки, полна ли очередь
+Queue(int n);
+~Queue();
+int get_front(); // Метод для получения первый элемент очереди
+int get_rear(); // Метод для получения последнего элемента очереди
+void enqueue(int value); // Метод для добавления элемента в конец очереди
+void display(); // Метод для отображения элементов в очереди
+bool is_empty(); // Метод для проверки, пуста ли очередь
+bool is_full(); // Метод для проверки, полна ли очередь
+void push(int value);  // Метод для добавления элемента в начало очереди
+int pop(); // Метод для удаления и возврата элемента из начала очереди
+
 };
 
-
 Queue::Queue(int n) {
-  size = n;
-  data = new int[size];
-  front = -1;
-  rear = -1;
+size = n;
+data = new int[size];
+front = -1;
+rear = -1;
 }
 
 Queue::~Queue() {
-  delete[] data;
+delete[] data;
 }
 
 int Queue::get_front() {
-  if (front == -1) { 
-    throw "Queue is Empty";
-  } else {
-    return data[front];
-  }
+return data[front];
 }
 
 int Queue::get_rear() {
-  if (rear == -1) { 
-    throw "Queue is Empty";
-  } else {
-    return data[rear];
-  }
+return data[rear];
 }
 
 void Queue::enqueue(int value) {
-  if (rear == size - 1) { 
-    throw "Queue is Full";
-  } else {
-    rear++;
-    data[rear] = value;
-    if (front == -1) { 
-      front = 0;
-    }
-  }
+rear++;
+data[rear] = value;
+if (front == -1) {
+front = 0;
+}
 }
 
-void Queue::dequeue() {
-  if (front == -1) { 
-    throw "Queue is Empty";
-  } else {
-    front++;
-    if (front > rear) { 
-      front = rear = -1;
-    }
-  }
-}
+
 
 void Queue::display() {
-  if (front == -1) { 
-    cout << "Queue is Empty" << endl;
-  } else {
-    for (int i = front; i <= rear; i++) {
-      cout << data[i] << " ";
-    }
-    cout << endl;
+  for (int i = front; i <= rear; i++) {
+    cout << data[i] << " ";
   }
+  cout << endl;
 }
 
 bool Queue::is_empty() {
@@ -90,6 +66,22 @@ bool Queue::is_full() {
 return (rear == size - 1);
 }
 
+void Queue::push(int value) {
+  rear++;
+  data[rear] = value;
+  if (front == -1) {
+    front = 0;
+  }
+}
+
+int Queue::pop() {
+  int value = data[front];
+  front++;
+  if (front > rear) {
+    front = rear = -1;
+  }
+  return value;
+}
 
 int main() {
 Queue q(4);
@@ -104,7 +96,7 @@ q.display();
 cout << "Front: " << q.get_front() << endl;
 cout << "Rear: " << q.get_rear() << endl;
 
-q.dequeue();
+q.pop();
 
 q.display();
 
