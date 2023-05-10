f7b5a1124d0c223fb5e1e2cc9faca50cca4cd433
Mashyanovk <114390812+Mashyanovk@users.noreply.github.com>
2023-05-10 21:54:40 +0300

    Update Aho_Corasick.h

--- Mashyanov_Konstantin/2sem/CourseWork/Aho_Corasick.h
+++ Mashyanov_Konstantin/2sem/CourseWork/Aho_Corasick.h
@@ -1,38 +1,38 @@
-#ifndef AHO_CORASICK_AHO_CORASICK_H
+#ifndef AHO_CORASICK_AHO_CORASICK_H  
 #define AHO_CORASICK_AHO_CORASICK_H
 
-#include <string>
-#include "vector"
-#include <map>
+#include <string>   
+#include "vector"   
+#include <map>      
 
-using namespace std;
+using namespace std;    
 
-typedef struct{
-    map<char, int> next_vertex;
-    int pattern_number;
-    int suffix_link;
-    map<char, int> move;
-    int suffix_final_link;
-    int parent;
-    char symbol;
-    bool isFinal;
-} Tree_Vertex;
+typedef struct{     
+    map<char, int> next_vertex;     // словарь, связывающий символ ребра с номером следующей вершины в дереве
+    int pattern_number;     // номер строки-образца в дереве
+    int suffix_link;        // суффиксная ссылка 
+    map<char, int> move;    // словарь, связывающий символ ребра с номером следующей вершины в дереве для строки-образца
+    int suffix_final_link;  // суффиксная ссылка на финальную вершину 
+    int parent;     // номер родительской вершины
+    char symbol;    // символ на ребре, соединяющем текущую вершину с родительской
+    bool is_final;   // является ли вершина финальной
+} tree_vertex;  
 
-class AC{
+class AC{    
 public:
-    void AddString(const string &str);
-    AC();
-    ~AC()= default;
-    void Find(const string &str);
+    AC();   
+    ~AC()= default;  
+    void add_string(const string &str); // метод добавления строки-образца в дерево
+    void find(const string &str);   // метод поиска всех строк-образцов в тексте
 private:
-    vector <Tree_Vertex> tree;
-    vector <string> pattern;
+    vector <tree_vertex> tree;  // вектор вершин дерева
+    vector <string> pattern;    // вектор строк-образцов
 
-    static Tree_Vertex MakeVertex(int parent, char symbol);
-    int GetSuffixLink(int vertex);
-    int GetMove(int vertex, char character);
-    int GetFinalSuffixLink(int vertex);
-    void Check(int vertex, int i);
+    static tree_vertex make_vertex(int parent, char symbol);   // статический метод создания вершины дерева
+    int get_suffix_link(int vertex);  // метод получения суффиксной ссылки для вершины дерева
+    int get_move(int vertex, char character);     // метод получения следующей вершины в дереве по символу на ребре
+    int get_final_suffix_link(int vertex);     // метод получения суффикcальной ссылки для вершины
+    void check(int vertex, int i);  //  метод проверки суффиксальных ссылок для вершины
 };
 
 #endif //AHO_CORASICK_AHO_CORASICK_H
