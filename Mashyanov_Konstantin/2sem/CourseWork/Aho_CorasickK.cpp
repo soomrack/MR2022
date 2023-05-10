4125b8087a0868a2ad4a2d7b8b38ad1c9aa05d8f
Mashyanovk <114390812+Mashyanovk@users.noreply.github.com>
2023-05-10 22:00:31 +0300

    Update Aho_CorasickK.cpp

--- Mashyanov_Konstantin/2sem/CourseWork/Aho_CorasickK.cpp
+++ Mashyanov_Konstantin/2sem/CourseWork/Aho_CorasickK.cpp
@@ -2,35 +2,35 @@
 #include "Aho_Corasick.h"
 
 AC::AC() {
-    this->tree.push_back(MakeVertex(0, '$'));
+    this->tree.push_back(make_vertex(0, '$'));
 }
 
-void AC::AddString(const string &str) {
+void AC::add_string(const string &str) {
     int num = 0;
     for (int i = 0; i < (int) str.length(); i++) {
         char symbol = str[i];
         if(this->tree[num].next_vertex.find(i) == this->tree[num].next_vertex.end()){
-            this->tree.push_back(MakeVertex(num, (char) symbol));
+            this->tree.push_back(make_vertex(num, (char) symbol));
             this->tree[num].next_vertex[symbol] = (int) this->tree.size() - 1;
         }
         num = this->tree[num].next_vertex[symbol];
     }
-    this->tree[num].isFinal = true;
+    this->tree[num].is_final = true;
     this->pattern.push_back(str);
     this->tree[num].pattern_number = (int) this->pattern.size() - 1;
 }
 
-void AC::Find(const string &str) {
+void AC::find(const string &str) {
     int u = 0;
     for (int i = 0; i < (int) str.length(); i++){
-        u = GetMove(u, (char) (str[i]));
-        this->Check(u, i + 1);
+        u = get_move(u, (char) (str[i]));
+        this->check(u, i + 1);
     }
 }
 
-Tree_Vertex AC::MakeVertex(int parent, char symbol) {
-    Tree_Vertex vertex;
-    vertex.isFinal = false;
+tree_vertex AC::make_vertex(int parent, char symbol) {
+    tree_vertex vertex;
+    vertex.is_final = false;
     vertex.suffix_link = -1;
     vertex.suffix_final_link = -1;
     vertex.parent = parent;
@@ -38,17 +38,17 @@ Tree_Vertex AC::MakeVertex(int parent, char symbol) {
     return vertex;
 }
 
-int AC::GetSuffixLink(int vertex) {
+int AC::get_suffix_link(int vertex) {
     if(this->tree[vertex].suffix_link == -1) {
         if (vertex == 0 || this->tree[vertex].parent == 0)
             this->tree[vertex].suffix_link = 0;
         else
-            this->tree[vertex].suffix_link = GetMove(GetSuffixLink(this->tree[vertex].parent), this->tree[vertex].symbol);
+            this->tree[vertex].suffix_link = get_move(get_suffix_link(this->tree[vertex].parent), this->tree[vertex].symbol);
     }
     return this->tree[vertex].suffix_link;
 }
 
-int AC::GetMove(int vertex, char symbol) {
+int AC::get_move(int vertex, char symbol) {
       if(this->tree[vertex].move.find(symbol) == this->tree[vertex].move.end()){
         if(this->tree[vertex].next_vertex.find(symbol) != this->tree[vertex].next_vertex.end()){
             this->tree[vertex].move[symbol] = this->tree[vertex].next_vertex[symbol];
@@ -56,15 +56,15 @@ int AC::GetMove(int vertex, char symbol) {
             if (vertex == 0)
                 this->tree[vertex].move[symbol] = 0;
             else
-                this->tree[vertex].move[symbol] = GetMove(GetSuffixLink(vertex), (char) symbol);
+                this->tree[vertex].move[symbol] = get_move(get_suffix_link(vertex), (char) symbol);
         }
     }
     return this->tree[vertex].move[symbol];
 }
 
-int AC::GetFinalSuffixLink(int vertex) {
+int AC::get_final_suffix_link(int vertex) {
     if(this->tree[vertex].suffix_final_link == -1){
-        int u = GetSuffixLink(vertex);
+        int u = get_suffix_link(vertex);
         if (u == 0){
             this->tree[vertex].suffix_final_link = 0;
         }
@@ -72,14 +72,14 @@ int AC::GetFinalSuffixLink(int vertex) {
             if(this->tree[u].isFinal)
                 this->tree[vertex].suffix_final_link = u;
             else
-                this->tree[vertex].suffix_final_link = GetFinalSuffixLink(u);
+                this->tree[vertex].suffix_final_link = get_final_suffix_link(u);
         }
     }
     return this->tree[vertex].suffix_final_link;
 }
 
-void AC::Check(int vertex, int i) {
-    for(int u = vertex; u != 0; u = GetFinalSuffixLink(u)){
+void AC::check(int vertex, int i) {
+    for(int u = vertex; u != 0; u = get_final_suffix_link(u)){
         if(this->tree[u].isFinal){
             std::cout << i - this->pattern[this->tree[u].pattern_number].length() + 1 << " " << this->pattern[this->tree[u].pattern_number] << std::endl;
         }
