972dd671ecd0d99550310015595ed11eac9ad4b6
orehalexeev <alexey.orehoff@gmail.com>
2023-05-15 02:17:37 +0300

    Changed graph to unweighted, other minor changes

--- Alexey_Orehov/abstract_data_types/graph/graph.h
+++ Alexey_Orehov/abstract_data_types/graph/graph.h
@@ -2,6 +2,8 @@
 #define MR2022_GRAPH_H
 
 #include "dynamic_array.h"
+#include <set>
+
 
 namespace Graph {
 
@@ -9,34 +11,52 @@ namespace Graph {
     class Node {
     private:
         T value;
-
         DynamicArray<Node<T>*> neighbors;
-        DynamicArray<unsigned int> weights;
+
     public:
         Node();
-        Node(const T& val);
+        explicit Node(const T& val);
         Node(const Node& node);
 
     };
 
     template<class T>
+    struct Edge {
+        Node<T>* orgn;
+        Node<T>* dest;
+    };
+
+    template<class T>
     class Graph {
     private:
-        DynamicArray<Node<T>> vertices;
+        DynamicArray<Node<T>*> vertices;
     public:
-        void add_node(const T& val);
+        Graph(DynamicArray<Edge<T>> edges);
 
     };
 
     template<typename T>
-    Node<T>::Node(): value(0), neighbors(DynamicArray<Node<T>*>()), weights(DynamicArray<unsigned int>()) {}
+    Node<T>::Node(): value(0), neighbors(DynamicArray<Node<T>*>()) {}
 
     template<typename T>
-    Node<T>::Node(const T &val): value(val), neighbors(DynamicArray<Node<T>*>()),
-    weights(DynamicArray<unsigned int>()) {}
+    Node<T>::Node(const T &val): value(val), neighbors(DynamicArray<Node<T>*>()) {}
 
     template<typename T>
-    Node<T>::Node(const Node<T> &node): value(node.value), neighbors(node.neighbors), weights(node.weights) {}
+    Node<T>::Node(const Node<T> &node): value(node.value), neighbors(node.neighbors) {}
+
+
+    template<typename T>
+    Graph<T>::Graph(DynamicArray<Edge<T>> edges) {
+        std::set<Node<T>*> counter;
+        for (int idx = 0; idx < edges.length(); idx++){
+            counter.insert(edges[idx].orgn);
+            counter.insert(edges[idx].dest);
+        }
+        vertices.resize(counter.size());
+        for (auto& el: counter) {
+            vertices.push_back(el);
+        }
+    }
 
 }
 
