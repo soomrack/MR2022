817a7b19108f1863a4513c7b13aa15a714845f98
Alexandra <alexunderbogdanka@gmail.com>
2023-05-22 11:23:48 +0300

    coursework update

---
+++ IgorVarlamov/Task_1_1.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <malloc.h>
+
+const unsigned int years = 20;
+const int annual_contrib = 60 * 1000 * 100;
+const int first_contrib = 300 * 1000;
+
+
+typedef struct person                                           // Создание структуры person
+{
+    const char* name;
+    unsigned long long bank;
+    unsigned long long revenue;
+    unsigned long spending;
+    unsigned long long house;
+    double rate;
+    double tax;
+}person;
+
+
+void init_Alice(person* Alice)                                  // Инициализация объекта структуры Alice
+{
+    Alice->name = "Alice";
+    Alice->bank = 1000 * 1000 * 100;
+    Alice->revenue = 150 * 1000 * 100;
+    Alice->house = 0;
+    Alice->spending = 40 * 1000 * 100;
+    Alice->rate = 0.08;
+}
+
+
+void init_Bob(person* Bob)                                      // Инициализация объекта структуры Bob
+{
+    Bob->name = "Bob";
+    Bob->tax = 0.001;
+    Bob->bank = 1000 * 1000 * 100 - first_contrib;
+    Bob->revenue = 150 * 1000 * 100;
+    Bob->house = 7000 * 1000 * 100;
+    Bob->spending = (long long)(10 * 1000 * 100 + annual_contrib + (double)(Bob)->house * Bob->tax/12);
+    Bob->rate = 0.08;
+}
+
+
+void month_pay (person* buyer)                                  // Месячный платеж
+{
+    buyer->bank = (unsigned long long)((double)(((buyer->bank) - buyer-> spending + buyer -> revenue))*
+            (1.f+ buyer->rate/12));
+}
+
+
+void live_changes_Alice(person* A, const unsigned int year)     // Изменения в доходах/ставке и итд...
+{ 
+    if (year == 10) 
+    {
+        A->revenue = 130 * 1000 * 100;
+    }
+    if (year == 7) 
+    {
+        A->rate = 0.11;
+    }
+}
+
+void live_changes_Bob(person* B, const unsigned int year)       // Изменения в доходах/ставке и итд... 
+{
+    if (year % 5 == 0)
+        B->house = (B->house * 3 / 2);
+    if (B->house / 100 < 50000 * 1000 && B->house / 100 > 20000 * 1000)
+        B->tax = 0.0015;
+    if (year == 12)
+        B->rate = 0.1;
+    B->spending = (long long)((double)(10* 1000 * 100 + annual_contrib +(double)(B)->house * B->tax/12));
+}
+
+
+void output ( person* client, unsigned int year)                // Вывод в консоль
+{
+    printf("%s\n",client ->name);
+    printf("current year - %d\n", year);
+    printf("bank_capital: %llu rubles and %llu cents\n", client->bank/100, client-> bank % 100);
+    printf("house_cost:%llu rubles and %llu cents\n", client->house/100, client-> house % 100);
+    printf("bank_rate:%f\n", client->rate);
+    printf("income: %llu and %llu cents\n", client->revenue/100, client-> revenue % 100);
+}
+
+void print_report ( person* A,  person* B)                      // Вывод в консоль результатов расчета
+{
+    if (B->bank + B->house > A->bank)
+        printf("Bob more profitable than Alice on %llu rubles!\n", ((B->bank + B->house) - A->bank) / 100);
+    else
+        printf("Alice more profitable than Bob on %llu rubles!\n", (A->bank - (B->bank + B->house)) / 100);
+}
+
+
+void main() 
+{
+    person Alice;
+    person Bob;
+
+    init_Alice(&Alice);
+    init_Bob(&Bob);
+
+    for (unsigned int year = 1; year <= years; year++) 
+    {
+        live_changes_Alice(&Alice,year);
+        live_changes_Bob(&Bob,year);
+        for (unsigned int month = 1; month <= 12; month++) 
+        {
+            month_pay(&Alice);
+            month_pay(&Bob);
+        }
+        output(&Alice,year);
+        output(&Bob,year);
+        print_report(&Alice,&Bob);
+    }
+}
+
---
+++ Maxim_Gavrilenko/Dynamic_array/Dynamic_array.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "dynamic_array.h"
+#include <ctime>
+template <typename T>
+void print (dynamic_array<T>& A) {
+    for (unsigned int number = 0; number < A.get_size(); number++){
+        std::cout << A[number] << "  ";
+    }
+    std::cout << "\n";
+}
+int main() {
+    try {
+        dynamic_array<double> A(10);;
+        for (unsigned i = 0; i < A.get_capacity(); i++)
+            A.insert(rand() % 10, i);
+        print(A);
+        std::cout << A.get_size() << std::endl;
+        A.replace(4, 3);
+        print(A);
+        A.search(4.08) ? std::cout << "YES" : std::cout << "NO";
+        std::cout << std::endl;
+        A.resize(12);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        print(A);
+        A.insert(3, 9);
+        A.insert(8, 2);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        A.insert(9, 2);
+        print(A);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+        A.resize(10);
+        print(A);
+        std::cout << "Unalloceted capacity: " << A.get_free_capacity() << std::endl;
+    }
+    catch (ARRAY_EXCEPTION &e)
+    {
+        std::cerr<<e.what()<<std::endl;
+    }
+    return 0;
+}
