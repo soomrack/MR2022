6f518838944866ee6c92f136308bcf35502dacc0
dmitriychanchikov <terkin1512@gmail.com>
2023-06-02 16:01:27 +0300

    Binary Tree (8-th version): fixed method "remove" and added comments

--- Chanchikov_Dmitriy/2 sem/Cpp/Binary Tree.cpp
+++ Chanchikov_Dmitriy/2 sem/Cpp/Binary Tree.cpp
@@ -2,20 +2,20 @@
 
 
 struct Node {
-    explicit Node(const double &value, Node *left = nullptr, Node *right = nullptr):
+    explicit Node(const double &value, Node* left = nullptr, Node* right = nullptr):
         data(value), left(left), right(right)  {}
 
     double data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
 };
 
 
 class BinaryTree {
 private:
-    Node *root;
+    Node* root;
 
-    static Node *find_parent_of_biggest_ptr(Node *subtree);
+    static Node* find_parent_of_biggest_ptr(Node *subtree);
     void destroy_rec(Node *node);
 
 public:
@@ -33,12 +33,10 @@ public:
 Node* BinaryTree::find_parent_of_biggest_ptr(Node *subtree) {
     Node* parent_of_biggest_ptr = nullptr;
     Node* current = subtree;
-
     while (current->right != nullptr) {
         parent_of_biggest_ptr = current;
         current = current->right;
     }
-
     return parent_of_biggest_ptr;
 }
 
@@ -46,18 +44,14 @@ Node* BinaryTree::find_parent_of_biggest_ptr(Node *subtree) {
 void BinaryTree::destroy_rec(Node *node) {
     if (node == nullptr)
         return;
-
     destroy_rec(node->left);
-
     destroy_rec(node->right);
-
     delete node;
 }
 
 
 void BinaryTree::insert(double value) {
-    auto **node = &root;
-
+    auto node = &root;
     while (*node != nullptr) {
         if (value == (*node)->data)
             return;
@@ -68,69 +62,67 @@ void BinaryTree::insert(double value) {
 
 
 bool BinaryTree::search(double value) {
-    auto** node = &root;
+    auto node = &root;
     while (*node != nullptr) {
         if ((*node)->data == value)
             return true;
         (*node)->data > value ? node = &(*node)->left : node = &(*node)->right;
-
     }
     return false;
 }
 
 
 void BinaryTree::remove(double value) {
-    auto node = &root;
-    auto current = node;
-
-    while (*current && (*current)->data != value)
+    auto node = &root;    // ук. на ук. на корневой узел
+    auto current = node;  // ук. на ук. на текущий узел
+    while (*current != nullptr && (*current)->data != value)  // ищем текущий узел, кот. соотв-ет value
         value < (*current)->data ? current = &(*current)->left : current = &(*current)->right;
-    if (*current == nullptr || *current == root)
-        return;
+    if (*current == nullptr || *current == root)  // если текущий узел не найден или явл. корневым узлом,
+        return;                                   // возврат
 
-    Node **node_to_remove = current;
-
-    if ((*node_to_remove)->left == nullptr){
-        *current = (*current)->right;
-        delete *node_to_remove;
+    Node* node_to_remove = *current;  // ук. на удаляемый узел
+    if (node_to_remove->left == nullptr) {  // если удаляемый узел не имеет левого потомка,
+        *current = (*current)->right;       // ук. на текущий узел = ук. на его правого потомка,
+        delete node_to_remove;              // удаляемый узел удаляется,
+        return;                             // возврат
+    }  /*
+    if (node_to_remove->right == nullptr) {  // если удаляемый узел не имеет правого потомка,
+        *current = (*current)->left;         // ук. на текущий узел = ук. на его левого потомка,
+        delete node_to_remove;               // удаляемый узел удаляется
         return;
+    }  */  // условие для правого потомка не нужно, т. к. будет обработано ниже
+
+    Node* parent_of_biggest_ptr = find_parent_of_biggest_ptr(node_to_remove->left);
+                                            // ук. на родителя самого большого узла в левом поддереве
+    if (parent_of_biggest_ptr == nullptr) { // если ук. на родителя не найден,
+        *current = node_to_remove->left;    // ук. на текущий узел = ук. на левого потомка удаляемого узла,
+    } else {                                              // в ином случае
+        *current = parent_of_biggest_ptr->right;          // ук. на текущий узел = ук. на правого потомка родителя,
+        parent_of_biggest_ptr->right = (*current)->left;  // ук. на правого п. родителя = ук. на левого п. текущего узла,
+        (*current)->left = node_to_remove->left;          // ук. на левого п. текущего узла = ук. на левого п. удаляемого узла
     }
-    if ((*node_to_remove)->right == nullptr) {
-        *current = (*current)->left;
-        delete *node_to_remove;
-        return;
-    }
-
-    Node *parent_of_biggest_ptr = find_parent_of_biggest_ptr((*node_to_remove)->left);
-
-    parent_of_biggest_ptr == nullptr ? *current = (*node_to_remove)->left : *current = parent_of_biggest_ptr->right;
-
-    (*current)->right = (*node_to_remove)->right;
-    *node_to_remove = *current;
-
-    delete *node_to_remove;
+    (*current)->right = node_to_remove->right;  // ук. на правого п. текущего узла = ук. на правого п. удаляемого узла
+    delete node_to_remove;  // удаляемый узел удаляется
 }
 
 
 void BinaryTree::print_rec(Node* node, int indent) {
     if (node == nullptr)
         return;
-
-    if (node->right != nullptr) {
+    if (node->right != nullptr)
         print_rec(node->right, indent + 4);
-    }
-
     for (int i = 0; i < indent; i++)
         std::cout << " ";
     std::cout << node->data << std::endl;
-
-    if (node->left != nullptr) {
+    if (node->left != nullptr)
         print_rec(node->left, indent + 4);
-    }
 }
 
 
 int main() {
+    double find = 8;
+    double del = find;
+
     BinaryTree tree;
 
     tree.insert(1);
@@ -147,23 +139,19 @@ int main() {
 
     tree.print();
 
-    if (tree.search(8) != 0)
-        std::cout << "Node found" << std::endl;
-    else
-        std::cout << "Node not found" << std::endl;
-
+    tree.search(find) != 0
+        ? std::cout << "Node " << find << " found" << std::endl
+        : std::cout << "Node " << find << " not found" << std::endl;
 
     std::cout << "================================" << std::endl;
 
-    tree.remove(8);
+    tree.remove(del);
 
-    //tree.print();
+    tree.print();
 
-    if (tree.search(8) != 0)
-        std::cout << "Node found" << std::endl;
-    else
-        std::cout << "Node not found" << std::endl;
-
+    tree.search(find) != 0
+        ? std::cout << "Node " << find << " found" << std::endl
+        : std::cout << "Node " << find << " not found" << std::endl;
 
     return 0;
 }
