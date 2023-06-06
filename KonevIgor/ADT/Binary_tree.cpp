5835bf8055863262e6aa684dd152a7cceffdba27
KonevIgor <konik562@yandex.ru>
2023-06-06 08:53:15 +0300

    Binary tree fixe (with deletion)

--- KonevIgor/ADT/Binary_tree.cpp
+++ KonevIgor/ADT/Binary_tree.cpp
@@ -1,7 +1,3 @@
-/*
-del O(h) Справа по дереву берем сразу левый или слева по дереву берем самый правый
- */
-
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -14,23 +10,25 @@ public:
     {}
 };
 
+template<typename N>
+class Tree_node {
+public:
+    N key;
+    Tree_node* left;
+    Tree_node* right;
+
+    explicit Tree_node(N data = N(),
+                       Tree_node* left = nullptr, Tree_node* right = nullptr) {
+        this->key = data;
+        this->left = left;
+        this->right = right;
+    }
+};
+
 template <typename T>
 class Binary_tree {
 
 private:
-    template<typename N>
-    struct Tree_node {
-        T key;
-        Tree_node* left;
-        Tree_node* right;
-
-        explicit Tree_node(N data = N(),
-                  Tree_node* left = nullptr, Tree_node* right = nullptr) {
-            this->key = data;
-            this->left = left;
-            this->right = right;
-        }
-    };
 
     Tree_node<T>* root;
     int height;
@@ -47,10 +45,13 @@ public:
 
     void add(T elem);
     bool is_find(T elem);
+    Tree_node<T>* find_elem(T elem);
+    Tree_node<T>* find_parent(T elem);
     void delete_tree();
-    void del(T element); // TODO:
+    void del(T element);
 
     unsigned get_height() {return height;}
+
 };
 
 template<typename T>
@@ -73,25 +74,46 @@ Binary_tree<T>::~Binary_tree() {
 
 template<typename T>
 void Binary_tree<T>::add(const T elem) {
-    Tree_node<T>** link = &root;
+    Tree_node<T>** temp = &root;
     int temp_height = 0;
-    while(*link != nullptr) {
-        link = elem < (*link)->key ? &((*link)->left) : &((*link)->right);
+    while(*temp != nullptr) {
+        temp = elem < (*temp)->key ? &((*temp)->left) : &((*temp)->right);
         temp_height++;
     }
-    *link = new Tree_node<T>(elem);
+    *temp = new Tree_node<T>(elem);
     height = std::max(height, temp_height);
 }
 
 // нужно ли бросать исключение, когда пытаемся внести существующий элемент?
 template<typename T>
 bool Binary_tree<T>::is_find(const T elem) {
-    Tree_node<T>* temp = root;
-    while (temp != nullptr) {
-        if (elem == temp->key) return true;
-        temp = (elem < temp->key) ? temp->left : temp->right;
+    Tree_node<T>** temp = &root;
+    while (*temp != nullptr) {
+        if (elem == (*temp)->key) return true;
+        *temp = (elem < (*temp)->key) ? (*temp)->left : (*temp)->right;
     }
     return false;
+}
+
+template<typename T>
+Tree_node<T>* Binary_tree<T>::find_parent(T elem) {
+    Tree_node<T>* temp = root;
+    while (temp != nullptr) {
+        if ((temp->left != nullptr) && (elem == temp->left->key)) return temp;
+        if ((temp->right != nullptr) && (elem == temp->right->key)) return temp;
+        temp = (elem < temp->key) ? temp->left : temp->right;
+    }
+    return nullptr;
+}
+
+template<typename T>
+Tree_node<T>* Binary_tree<T>::find_elem(T elem) {
+    Tree_node<T>* temp = root;
+    while (temp != nullptr) {
+        if (elem == temp->key) return temp;
+        temp = (elem < temp->key) ? temp->left : temp->right;
+    }
+    return nullptr;
 }
 
 template<typename T>
@@ -115,7 +137,7 @@ Binary_tree<T> &Binary_tree<T>::operator=(const Binary_tree& other) {
 }
 
 template<typename T>
-void Binary_tree<T>::add_child(Binary_tree::Tree_node<T> * node) {
+void Binary_tree<T>::add_child(Tree_node<T> * node) {
     if (node->left != nullptr) {
         add(node->left->key);
         add_child(node->left);
@@ -127,7 +149,7 @@ void Binary_tree<T>::add_child(Binary_tree::Tree_node<T> * node) {
 }
 
 template<typename T>
-void Binary_tree<T>::delete_child(Binary_tree::Tree_node<T>* node) {
+void Binary_tree<T>::delete_child(Tree_node<T>* node) {
     if ((node->left != nullptr) || (node->right != nullptr)) height--;
     if (node->left != nullptr) {
         delete_child(node->left);
@@ -149,20 +171,54 @@ void Binary_tree<T>::delete_tree() {
 
 template<typename T>
 void Binary_tree<T>::del(const T element) {
+    Tree_node<T>* temp = find_elem(element);
 
+    if ((temp == root) && (temp->right == nullptr || temp->left == nullptr)) {
+        if (temp->right != nullptr) {
+            root->right->left = root->left;
+            root = temp->right;
+        } else {
+            root->left->right = root->right;
+            root = temp->left;
+        }
+        return;
+    }
+
+    if (temp->left == nullptr && temp->right == nullptr) {
+        Tree_node<T>* parent = find_parent(element);
+        if (parent->key < element) {parent->right = nullptr; return;}
+        if (parent->key > element) {parent->left = nullptr; return;}
+    }
+
+    if (temp->left == nullptr || temp->right == nullptr) {
+        Tree_node<T>* parent = find_parent(element);
+        if (parent->key < element) {
+            parent->right = (temp->left == nullptr) ? temp->right : temp->left;
+            return;}
+        if (parent->key > element) {
+            parent->left = (temp->left == nullptr) ? temp->right : temp->left;
+            return;}
+    }
+
+    Tree_node<T>* temp_2 = temp->left;
+    while (temp_2->right != nullptr) temp_2 = temp_2->right;
+    del(temp_2->key);
+    temp->key = temp_2->key;
 }
 
 int main() {
     Binary_tree<int> test(10);
-    test.add(15);
-    test.add(5);
-    test.add(1);
-    test.add(7);
-    test.add(100);
-    test.add(12);
-    test.add(6);
+//    test.add(15);
+//    test.add(5);
+//    test.add(1);
+//    test.add(7);
+//    test.add(100);
+//    test.add(12);
+//    test.add(6);
+//    test.add(2);
     Binary_tree<int> test2(test);
     test2.delete_tree();
-    std::cout << test.get_height() << "\t" << test.is_find(100) << std::endl;
+    std::cout << test.get_height() << "\t" << test.is_find(10) << std::endl;
+    test.del(10);
     return 0;
 }
