a63548c4484acb38657bc65a22dcb59060f771c4
Ivan <124557018+Hvbbbb@users.noreply.github.com>
2023-09-29 12:53:35 +0300

    Add files via upload

---
+++ Андреев_Иван/B-TREE/main.cpp
@@ -0,0 +1,43 @@
+#include "btree.h"
+#include <iostream>
+
+int main () {
+
+    Tree bst;
+    int a;
+    bst.insert(55);
+    bst.insert(40);
+    bst.insert(65);
+    bst.insert(60);
+    bst.insert(75);
+    bst.insert(56);
+    bst.insert(41);
+    bst.insert(555);
+    bst.insert(42);
+    bst.insert(57);
+    bst.insert(5);
+    bst.insert(1);
+    bst.insert(69);
+    bst.insert(90);
+    bst.insert(91);
+    bst.insert(68);
+    bst.insert(85);
+    bst.insert(86);
+    bst.insert(7);
+    bst.insert(6);
+    bst.print();
+
+    std::cout << "\n"
+              << "After deleting" << "\n";
+    bst.del(75);
+
+
+    bst.print();
+
+    a = bst.search(960);
+    std::cout << "\n"
+              << a << "\n";
+
+
+    return 0;
+}
